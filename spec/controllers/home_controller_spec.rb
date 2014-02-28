7127767bb178844d41debfeec4e18098aa6d0b03
cleverapollo <cleverapollo@gmail.com>
2014-02-28 17:58:48 +0400

    More specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe HomeController do
-  it 'pending'
+  describe 'GET index' do
+    it 'responds with 200' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
 end
--- spec/services/mahout_service_spec.rb
+++ spec/services/mahout_service_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe MahoutService do
-  it 'pending'
+  describe '.recommendations' do
+    before do
+      @tunnel = Object.new
+      allow(@tunnel).to receive(:recommend_block) {
+        [1, 2, 3]
+      }
+
+      allow(BrB::Tunnel).to receive(:create) {
+        @tunnel
+      }
+    end
+
+    it 'calls recommend_block on brb tunnel' do
+      MahoutService.recommendations(1, {})
+
+      expect(@tunnel).to have_received(:recommend_block).with(1, {})
+    end
+  end
 end
