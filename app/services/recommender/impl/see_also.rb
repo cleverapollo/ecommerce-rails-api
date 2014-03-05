962567515719c571bb6731023bb015d52e82c36b
cleverapollo <cleverapollo@gmail.com>
2014-03-05 16:28:37 +0400

    Recommenders

--- app/services/recommender/impl/see_also.rb
+++ app/services/recommender/impl/see_also.rb
@@ -1,8 +1,28 @@
 module Recommender
   module Impl
-    class SeeAlso < Recommender::Filtered
-      def items_to_estimate
-        params.cart_item_ids
+    class SeeAlso < Recommender::Weighted
+      LIMIT = 20
+
+      def shared_orders
+        OrderItem.where(item_id: params.cart_item_ids).pluck(:order_id)
+      end
+
+      def excluded_items
+        ids = []
+        ids += Action.where('purchase_count > 0').where(user_id: params.user.id).pluck(:item_id)
+        ids += params.cart_item_ids
+        ids.uniq.compact
+      end
+
+      def items_to_weight
+        items = OrderItem.select('item_id')
+                         .where('order_id IN (?)', shared_orders)
+                         .where('item_id NOT IN (?)', excluded_items)
+                         .group('item_id')
+                         .order('count(item_id) desc')
+                         .limit(LIMIT)
+
+        items.map(&:item_id)
       end
     end
   end
