6cf2c78a65421a1acb00eeec54a611f3ed5e0422
cleverapollo <cleverapollo@gmail.com>
2014-03-07 12:20:33 +0400

    Rollbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::API
     end
 
     def respond_with_client_error(exception)
+      Rollbar.report_exception(exception)
       render json: { status: 'error', message: exception.to_s }, status: 400
     end
 end
