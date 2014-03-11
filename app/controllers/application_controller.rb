f84ecfe3ad0434d8e2b272179a86beb623a36306
cleverapollo <cleverapollo@gmail.com>
2014-03-11 18:25:24 +0400

    Ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::API
+  before_filter :set_headers
+
   protected
+    def set_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+
     def respond_with_success
       render json: { status: 'success' }
     end
