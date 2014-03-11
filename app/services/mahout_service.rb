b5d62efadc6e9565e1bcaf1c6e5bc19a3aa84f1f
cleverapollo <cleverapollo@gmail.com>
2014-03-11 19:08:55 +0400

    Messages

--- app/services/mahout_service.rb
+++ app/services/mahout_service.rb
@@ -8,13 +8,21 @@ class MahoutService
   end
 
   def user_based(user_id, options)
-    return [] unless tunnel_active?
-    tunnel.user_based_block(user_id, options)
+    if tunnel_active?
+      tunnel.user_based_block(user_id, options)
+    else
+      return []
+      puts "Tunnel inactive!"
+    end
   end
 
   def item_based_weight(user_id, options)
-    return options[:weight].slice(0, options[:limit]) unless tunnel_active?
-    tunnel.item_based_weight_block(user_id, options)
+    if tunnel_active?
+      tunnel.item_based_weight_block(user_id, options)
+    else
+      puts "Tunnel inactive!"
+      return options[:weight].slice(0, options[:limit])
+    end
   end
 
   private
