15c1cc0f5ee8108e44d605e73c7b618a44732df4
cleverapollo <cleverapollo@gmail.com>
2014-03-12 17:36:53 +0400

    Fix

--- app/services/recommender/impl/interesting.rb
+++ app/services/recommender/impl/interesting.rb
@@ -1,6 +1,13 @@
 module Recommender
   module Impl
     class Interesting < Recommender::UserBased
+      def recommended_ids
+        if params.user.present? and params.user.actions.where(shop_id: params.shop.id).count > 4
+          super
+        end
+
+        []
+      end
     end
   end
 end
