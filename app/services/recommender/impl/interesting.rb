f377ee0c0bf5e59011c2fe5b8520db510eb05e1e
cleverapollo <cleverapollo@gmail.com>
2014-03-13 13:33:50 +0400

    Facepalm.jpg

--- app/services/recommender/impl/interesting.rb
+++ app/services/recommender/impl/interesting.rb
@@ -3,7 +3,7 @@ module Recommender
     class Interesting < Recommender::UserBased
       def recommended_ids
         if params.user.present? and params.user.actions.select(:id).where(shop_id: params.shop.id).limit(4).size == 4
-          super
+          return super
         end
 
         []
