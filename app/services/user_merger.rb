1a165fbf1055c14e5a875be7499a9042c9380de6
cleverapollo <cleverapollo@gmail.com>
2014-03-17 11:56:31 +0400

    Rollbar #37 #38

--- app/services/user_merger.rb
+++ app/services/user_merger.rb
@@ -12,11 +12,11 @@ class UserMerger
             view_count: master_action.view_count + slave_action.view_count,
             cart_count: master_action.cart_count + slave_action.cart_count,
             purchase_count: master_action.purchase_count + slave_action.purchase_count,
-            view_date: [master_action.view_date, slave_action.view_date].max,
-            cart_date: [master_action.cart_date, slave_action.cart_date].max,
-            purchase_date: [master_action.purchase_date, slave_action.purchase_date].max,
-            rating: [master_action.rating, slave_action.rating].max,
-            timestamp: [master_action.timestamp, slave_action.timestamp].max,
+            view_date: [master_action.view_date, slave_action.view_date].compact.max,
+            cart_date: [master_action.cart_date, slave_action.cart_date].compact.max,
+            purchase_date: [master_action.purchase_date, slave_action.purchase_date].compact.max,
+            rating: [master_action.rating, slave_action.rating].compact.max,
+            timestamp: [master_action.timestamp, slave_action.timestamp].compact.max,
             recommended_by: master_action.recommended_by || slave_action.recommended_by
 
           slave_action.destroy
