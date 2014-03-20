05fc976eccf0b74fea55631287547b55c5a6d214
cleverapollo <cleverapollo@gmail.com>
2014-03-20 12:25:51 +0400

    Rollbar #41

--- app/controllers/init_controller.rb
+++ app/controllers/init_controller.rb
@@ -4,7 +4,7 @@ class InitController < ApplicationController
   def init_script
     session_id = cookies[Rees46.cookie_name] || params[Rees46.cookie_name]
 
-    @session = Session.fetch(uniqid: session_id, useragent: request.env['HTTP_USER_AGENT'].truncate(250))
+    @session = Session.fetch(uniqid: session_id, useragent: user_agent)
 
     cookies.delete([Rees46.cookie_name])
     cookies.permanent[Rees46.cookie_name] = @session.uniqid
@@ -16,5 +16,13 @@ class InitController < ApplicationController
 
   def init_server_string(session)
     "REES46.initServer('#{session.uniqid}', '#{Rees46.base_url}', #{session.user.ab_testing_group});"
+  end
+
+  def user_agent
+    if request.env['HTTP_USER_AGENT'].present?
+      request.env['HTTP_USER_AGENT'].truncate(250)
+    else
+      ''
+    end
   end
 end
