06c0245c6af07f82ffc9c0c3abe97b09bfbf4293
cleverapollo <cleverapollo@gmail.com>
2014-03-31 13:17:33 +0400

    Fix

--- app/services/mahout_preferences.rb
+++ app/services/mahout_preferences.rb
@@ -7,7 +7,7 @@ class MahoutPreferences
   def fetch(limit = 10)
     result = []
 
-    result << Action.where(user_id: @user_id, shop_id: @shop_id).order('rating desc').order('timestamp desc').limit(10).pluck(:item_id)
+    result += Action.where(user_id: @user_id, shop_id: @shop_id).order('rating desc').order('timestamp desc').limit(10).pluck(:item_id)
 
     result
   end
