af84358b87ef992f41b7b495ccc45796d4182692
cleverapollo <cleverapollo@gmail.com>
2014-04-01 12:30:37 +0400

    Fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,11 @@ class Item < ActiveRecord::Base
   class << self
     def fetch(shop_id, item)
       i = find_or_initialize_by(shop_id: shop_id, uniqid: item.uniqid)
-      i.assign_attributes(category_uniqid: item.category_uniqid, price: item.price, is_available: item.is_available)
+      i.assign_attributes \
+                          category_uniqid: item.category_uniqid.present? ? item.category_uniqid : i.category_uniqid,
+                          price: item.price.present? ? item.price : i.price,
+                          is_available: item.is_available
+
       i.amount = item.amount
       if i.persisted? and i.changed?
         Action.where(item_id: i.id).update_all(is_available: i.is_available, price: i.price, category_uniqid: i.category_uniqid)
