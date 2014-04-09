b42070cbb3cc6d336a39adfa1ea93836da6204f8
cleverapollo <cleverapollo@gmail.com>
2014-04-09 11:23:08 +0400

    Heartbleed

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,6 +1,6 @@
 require 'rollbar/rails'
 Rollbar.configure do |config|
-  config.access_token = '4899eb45828d4b62932213125ddf8a71'
+  config.access_token = '8b197bc247a844278f109dbd06ad2e66'
 
   if Rails.env.development? or Rails.env.test?
     config.enabled = false
