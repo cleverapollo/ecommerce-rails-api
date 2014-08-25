ad2fa333871bd78f67d932bcf0373e9e76dd367b
cleverapollo <cleverapollo@gmail.com>
2014-08-25 14:12:31 +0400

    REES-284 Dropped cart

--- app/services/recommender/impl/interesting.rb
+++ app/services/recommender/impl/interesting.rb
@@ -1,16 +1,31 @@
 module Recommender
   module Impl
     class Interesting < Recommender::UserBased
+      def check_params
+        params.user.present?
+      end
+
       def recommended_ids
-        if params.user.present?
-          res = super
-          if params.item.present?
-            res = res - [params.item.id]
-          end
-          return res
+        res = super
+        if params.item.present?
+          res = res - [params.item.id]
         end
 
-        []
+        id = not_bought_but_carted_id
+        if id.present? && id != params.item.try(:id)
+          res = res.insert(2, id)
+          res.compact!
+        end
+
+        res
+      end
+
+      def not_bought_but_carted_id 
+        relation = params.user.actions.where(shop_id: params.shop.id)
+        relation = relation.where("rating = '#{Actions::Cart::RATING}'::real")
+        relation = relation.where('cart_date <= ?', 30.minutes.ago)
+        relation = relation.where('cart_date >= ?', 24.hours.ago)
+        relation.limit(1).pluck(:item_id).first
       end
     end
   end
