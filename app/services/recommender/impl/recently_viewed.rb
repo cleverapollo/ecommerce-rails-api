1024bad52456c962efa44c8bf32b02544cf7ab55
cleverapollo <cleverapollo@gmail.com>
2014-09-15 15:12:31 +0400

    Recently viewed: exclude cart

--- app/services/recommender/impl/recently_viewed.rb
+++ app/services/recommender/impl/recently_viewed.rb
@@ -5,7 +5,7 @@ module Recommender
 
       def recommended_ids
         relation = shop.actions.where(user: user).where('view_count > 0')
-        relation = relation.where.not(item: item) if item.present?
+        relation = relation.where.not(item_id: excluded_items_ids)
         relation.order('view_date DESC').limit(LIMIT).pluck(:item_id)
       end
     end
