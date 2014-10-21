1fbc7b98069adf8219dfee502f338f5727a21cef
cleverapollo <cleverapollo@gmail.com>
2014-10-21 16:13:42 +0400

    REES-550 Documentation for ParamsSanitizer

--- lib/params_sanitizer.rb
+++ lib/params_sanitizer.rb
@@ -2,6 +2,11 @@
 # Класс, обрабатывающий приходящие параметры от битых данных
 #
 class ParamsSanitizer
+  # Рекурсивная функция обхода входящего хэша и обработки всех строк в нем.
+  # Изменяет объект по ссылке.
+  # Умеет работать с массивами и вложенными хэшами.
+  #
+  # @param params [Hash] входящий хэш параметров
   def self.sanitize!(params)
     if params.is_a? Hash
       params.each do |key, value|
@@ -18,6 +23,11 @@ class ParamsSanitizer
     end
   end
 
+  # Чистит входящую строку от битых символов.
+  # Если передана не строка, то возращает переданное значение обратно
+  #
+  # @param value [Object] входящий параметр любого типа
+  # @return [Object] возвращается объект того же типа
   def self.sanitize_value(value)
     if value.is_a? String
       value.encode('UTF-8', {:invalid => :replace, :undef => :replace, :replace => ''})
