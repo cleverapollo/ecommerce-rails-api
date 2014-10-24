36474f91792d20f633894b512ea97b2ee4cb0c72
cleverapollo <cleverapollo@gmail.com>
2014-10-24 15:22:31 +0400

    Add item_id index to order_items

---
+++ db/migrate/20141024112136_add_item_id_index_to_order_items.rb
@@ -0,0 +1,5 @@
+class AddItemIdIndexToOrderItems < ActiveRecord::Migration
+  def change
+    add_index :order_items, :item_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016114907) do
+ActiveRecord::Schema.define(version: 20141024112136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -232,6 +232,7 @@ ActiveRecord::Schema.define(version: 20141016114907) do
     t.string  "recommended_by"
   end
 
+  add_index "order_items", ["item_id"], name: "index_order_items_on_item_id", using: :btree
   add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
 
   create_table "orders", force: true do |t|
