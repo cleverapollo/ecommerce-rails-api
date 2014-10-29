7aa330717927d6b07b27310ef1b23e944d851122
cleverapollo <cleverapollo@gmail.com>
2014-10-29 11:59:34 +0300

    REES-537 Fixed limit in recently_viewed

--- app/services/recommender/impl/recently_viewed.rb
+++ app/services/recommender/impl/recently_viewed.rb
@@ -1,12 +1,14 @@
 module Recommender
   module Impl
+    ##
+    # Класс, реализующий рекомендации по алгоритму "Вы недавно смотрели"
+    #
     class RecentlyViewed < Recommender::Raw
-      LIMIT = 10
-
+      # Суть крайне проста - найти N последних просмотренных пользователем товаров
       def recommended_ids
         relation = shop.actions.where(user: user).where('view_count > 0')
         relation = relation.where.not(item_id: excluded_items_ids)
-        relation.order('view_date DESC').limit(LIMIT).pluck(:item_id)
+        relation.order('view_date DESC').limit(limit).pluck(:item_id)
       end
     end
   end
