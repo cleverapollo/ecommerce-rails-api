e69d089618648b57f05a05b672b6fb00dae74286
cleverapollo <cleverapollo@gmail.com>
2014-11-05 18:08:10 +0300

    Fixed error on see_also when item in cart is unknown

--- app/services/recommender/impl/see_also.rb
+++ app/services/recommender/impl/see_also.rb
@@ -1,6 +1,9 @@
 module Recommender
   module Impl
     class SeeAlso < AlsoBought
+      def check_params!
+      end
+
       def items_which_cart_to_analyze
         params.cart_item_ids
       end
