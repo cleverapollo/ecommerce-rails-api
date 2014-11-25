4294aff26008f483bf284429b715b622deeccfe8
cleverapollo <cleverapollo@gmail.com>
2014-11-25 15:26:40 +0300

    Algorithm tuning

--- app/services/recommender/user_based.rb
+++ app/services/recommender/user_based.rb
@@ -5,13 +5,25 @@ module Recommender
       ms.open
 
       result = if ms.tunnel && ms.tunnel.active?
-        ms.user_based(params.user.id,
+        r = ms.user_based(params.user.id,
                       params.shop.id,
                       params.item_id,
                       include: items_in_shop,
                       include: [],
                       exclude: excluded_items_ids,
                       limit: params.limit)
+
+        if r.none?
+          r = ms.user_based(params.user.id,
+                      params.shop.id,
+                      nil,
+                      include: items_in_shop,
+                      include: [],
+                      exclude: excluded_items_ids,
+                      limit: params.limit)
+        end
+
+        r
       else
         []
       end
