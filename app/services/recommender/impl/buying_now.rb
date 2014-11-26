3ec91e3e5ed606fc956284687d9e22008b4ef488
cleverapollo <cleverapollo@gmail.com>
2014-11-26 14:43:46 +0300

    Algorythm tuning

--- app/services/recommender/impl/buying_now.rb
+++ app/services/recommender/impl/buying_now.rb
@@ -6,13 +6,15 @@ module Recommender
       def items_to_weight
         min_date = 1.day.ago.to_i
 
-        # Выводит топ товаров за последние сутки по среднему рейтингу.
+        # Выводит топ товаров за последние сутки.
         # Те, что чаще покупаются - будут выше.
         # Если покупок нет, все равно будет работать.
         result = shop.actions.available.where('timestamp >= ?', min_date)
         result = result.where.not(item_id: excluded_items_ids)
         result = result.in_locations(locations)
-        result = result.group(:item_id).order('AVG(rating) DESC, SUM(rating) DESC').limit(LIMIT).pluck(:item_id)
+        result = result.group(:item_id)
+        result = result.order('SUM(purchase_count) DESC, SUM(view_count) DESC')
+        result = result.limit(LIMIT).pluck(:item_id)
         unless shop.strict_recommendations?
           result = inject_random_items(result)
         end
