fa4c4670b3c567ac9ef1f9a7c92c255a970cb371
cleverapollo <cleverapollo@gmail.com>
2014-12-02 11:38:41 +0300

    REES-723 Тестовое письмо

--- app/workers/mailing_batch_worker.rb
+++ app/workers/mailing_batch_worker.rb
@@ -2,31 +2,42 @@ class MailingBatchWorker
   include Sidekiq::Worker
   sidekiq_options retry: false, queue: 'long'
 
-  def perform(shop_id, mailing_id, start_id, end_id)
-    shop = Shop.find(shop_id)
-    mailing = DigestMailing.find(mailing_id)
+  def perform(params, test_mail = nil)
+    shop = Shop.find(params['shop_id'])
+    mailing = DigestMailing.find(params['mailing_id'])
     mailing_setting = shop.digest_mailing_setting
     if mailing_setting.on?
       recommendations_count = mailing.template.scan(/\{\{ item \}\}/).size
       DigestMailingRecommendationsCalculator.create(shop, recommendations_count) do |calculator|
-        shop.audiences.enabled.where(id: start_id..end_id).each do |audience|
-          audience.try_to_attach_to_user!
-          recommendations = calculator.recommendations_for(audience.user)
-          items = replace_items_to_template(recommendations, mailing.item_template)
-          body = mailing.template.dup
-          items.each do |item_str|
-            body.sub!(/\{\{ item \}\}/, item_str)
+        if test_mail.nil?
+          shop.audiences.enabled.where(id: params['start_id']..params['end_id']).each do |audience|
+            audience.try_to_attach_to_user!
+            recommendations = calculator.recommendations_for(audience.user)
+            sent_mail(audience.email, mailing.subject, mailing_setting.sender, body_for_sent(recommendations, mailing.item_template, mailing.template))
+            ActiveRecord::Base.connection.execute("UPDATE digest_mailings SET was_sent = was_sent + 1 WHERE id = #{mailing.id}")
           end
-          Mailer.digest(
-            email: audience.email,
-            subject: mailing.subject,
-            send_from: mailing_setting.sender,
-            body: body
-          ).deliver
-          ActiveRecord::Base.connection.execute("UPDATE digest_mailings SET was_sent = was_sent + 1 WHERE id = #{mailing.id}")
+        else
+          recommendations = calculator.recommendations_for(nil)
+          sent_mail(test_mail, mailing.subject, mailing_setting.sender, body_for_sent(recommendations, mailing.item_template, mailing.template))
         end
       end
     end
+  end
+
+  def sent_mail(to, subject, from, body)
+    Mailer.digest(
+      email: to,
+      subject: subject,
+      send_from: from,
+      body: body
+    ).deliver
+  end
+
+  def body_for_sent(recommendations, item_template, body_template)
+    items = replace_items_to_template(recommendations, item_template)
+    body = body_template.dup
+    items.each { |item_str| body.sub!(/\{\{ item \}\}/, item_str) }
+    body
   end
 
   def replace_items_to_template(recommendations, template)
--- app/workers/mailing_launch_worker.rb
+++ app/workers/mailing_launch_worker.rb
@@ -6,16 +6,20 @@ class MailingLaunchWorker
 
   def perform(params)
     fetch_and_authenticate_shop(params.fetch('shop_id'), params.fetch('shop_secret'))
-    process_mailing(params.fetch('id'))
+    process_mailing(params.fetch('id'), params.fetch('test_email'))
   end
 
   def fetch_and_authenticate_shop(uniqid, secret)
     @shop = Shop.find_by!(uniqid: uniqid, secret: secret)
   end
 
-  def process_mailing(mailing_id)
-    shop.audiences.enabled.each_batch_with_start_end_id do |start_id, end_id|
-      MailingBatchWorker.perform_async(shop.id, mailing_id, start_id, end_id)
+  def process_mailing(mailing_id, test_mail)
+    if test_mail.present?
+      MailingBatchWorker.perform_async({ shop_id: shop.id, mailing_id: mailing_id }, test_mail)
+    else
+      shop.audiences.enabled.each_batch_with_start_end_id do |start_id, end_id|
+        MailingBatchWorker.perform_async(shop_id: shop.id, mailing_id: mailing_id, start_id: start_id, end_id: end_id)
+      end
     end
   end
 end
