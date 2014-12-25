0e837b42849873dc7606c7aa5c20cb697debd502
cleverapollo <cleverapollo@gmail.com>
2014-12-25 17:52:21 +0300

    Fix

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -76,6 +76,6 @@ class Shop < ActiveRecord::Base
   end
 
   def show_promotion?
-    self.manual == false && (self.paid == false || self.plan_id.present?)
+    self.manual == false && (self.paid == false || self.plan_id.blank?)
   end
 end
