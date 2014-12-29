3e8bce32a8b08506e18b69ce112971f08344cedd
cleverapollo <cleverapollo@gmail.com>
2014-12-29 17:05:13 +0300

    Beacons fix

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -15,7 +15,7 @@ class BeaconsController < ApplicationController
       beacon_message.params = params
       beacon_message.save!
 
-      if params[:type] == 'lead' && shop.beacon_messages.with_notifications.where('created_at >= ?', 2.weeks.ago).none?
+      if params[:type] == 'lead' && shop.beacon_messages.where(user_id: user.id).with_notifications.where('created_at >= ?', 2.weeks.ago).none?
         beacon_message.update(notified: true)
         result = {
           image: 'http://cdn.rees46.com/bk.gif',
