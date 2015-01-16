3aaa77397fdd6dcf8cbefcfd47bfbe6e15e0aeb1
cleverapollo <cleverapollo@gmail.com>
2015-01-16 18:12:46 +0300

    Fast fix

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -81,6 +81,7 @@ class Shop < ActiveRecord::Base
   end
 
   def subscriptions_enabled?
-    subscriptions_settings.present? && subscriptions_settings.enabled?
+    false
+    #subscriptions_settings.present? && subscriptions_settings.enabled?
   end
 end
