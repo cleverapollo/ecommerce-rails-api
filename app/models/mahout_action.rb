f361c48a7b9b9f0b1880b58b037b890bfcd3351b
cleverapollo <cleverapollo@gmail.com>
2015-02-06 17:31:09 +0300

    MahoutAction merger

--- app/models/mahout_action.rb
+++ app/models/mahout_action.rb
@@ -1,10 +1,22 @@
 class MahoutAction < ActiveRecord::Base
-  include UserLinkable
-
   before_create :assign_timestamp
 
   belongs_to :item
   belongs_to :shop
+  belongs_to :user
+
+
+  class << self
+    def relink_user(options = {})
+      where(user_id: options.fetch(:from).id).find_each do |m_a|
+        if MahoutAction.where(item_id: item.id, user_id: options.fetch(:to).id).limit(1).blank?
+          m_a.update_columns(user_id: options.fetch(:to).id)
+        else
+          m_a.delete
+        end
+      end
+    end
+  end
 
   protected
     def assign_timestamp
