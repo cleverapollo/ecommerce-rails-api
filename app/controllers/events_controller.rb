55f49b31edd99cb4ae826e414e2f97edc661e934
cleverapollo <cleverapollo@gmail.com>
2015-03-02 16:07:25 +0300

    Remove lock when pushing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,19 +4,12 @@ class EventsController < ApplicationController
 
     extract_legacy_event_name if params[:event].blank?
 
-    s = Session.find_by(code: params[:ssid])
-    raise UserFetcher::SessionNotFoundError if s.blank?
-    s.pushing_lock.lock {
-      ActiveRecord::Base.transaction {
-        parameters = ActionPush::Params.extract(params)
-        ActionPush::Processor.new(parameters).process
-      }
+    ActiveRecord::Base.transaction {
+      parameters = ActionPush::Params.extract(params)
+      ActionPush::Processor.new(parameters).process
     }
 
     respond_with_success
-  rescue Redis::Lock::LockTimeout => e
-    log_client_error(e)
-    respond_with_client_error(e)
   rescue ActionPush::Error => e
     log_client_error(e)
     respond_with_client_error(e)
