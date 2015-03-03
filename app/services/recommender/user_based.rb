94cde58935b40d8bddd67988f080209929d410b8
cleverapollo <cleverapollo@gmail.com>
2015-03-03 18:30:26 +0300

    Gender in interesting

--- app/services/recommender/user_based.rb
+++ app/services/recommender/user_based.rb
@@ -1,11 +1,27 @@
 module Recommender
   class UserBased < Base
+    def items_to_recommend
+      if shop.sectoral_algorythms_available?
+        result = super
+        if shop.category.wear?
+          if item.present?
+            if item.custom_attributes['gender'].present?
+              result = result.by_ca(gender: item.custom_attributes['gender'])
+            end
+          end
+        end
+        result
+      else
+        super
+      end
+    end
+
     def recommended_ids
       ms = MahoutService.new
       ms.open
 
       result = if ms.tunnel && ms.tunnel.active?
-        items_to_include = items_in_shop
+        items_to_include = items_to_recommend
         if recommend_only_widgetable?
           items_to_include = items_to_include.merge(Item.widgetable)
         end
