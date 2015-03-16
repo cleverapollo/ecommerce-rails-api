ed73ffc489557eee3895cbb7ff677f9000094829
cleverapollo <cleverapollo@gmail.com>
2015-03-16 15:08:11 +0300

    Diagnostics code

--- app/services/trigger_mailings/clients_processor.rb
+++ app/services/trigger_mailings/clients_processor.rb
@@ -9,14 +9,18 @@ module TriggerMailings
         Shop.unrestricted.with_enabled_triggers.each do |shop|
           TriggerMailings::TriggerDetector.for(shop) do |trigger_detector|
             shop.clients.suitable_for_trigger_mailings.find_each do |client|
-              if client.last_trigger_mail_sent_at.present? &&
-                 client.last_trigger_mail_sent_at >= 2.weeks.ago
-                 next
-              end
+              begin
+                if client.last_trigger_mail_sent_at.present? &&
+                   client.last_trigger_mail_sent_at >= 2.weeks.ago
+                   next
+                end
 
-              if trigger = trigger_detector.detect(client)
-                TriggerMailings::Letter.new(client, trigger).send
-                client.update_columns(last_trigger_mail_sent_at: Time.now)
+                if trigger = trigger_detector.detect(client)
+                  TriggerMailings::Letter.new(client, trigger).send
+                  client.update_columns(last_trigger_mail_sent_at: Time.now)
+                end
+              rescue StandardError => e
+                Rollbar.error(e, client_id: client.try(:id), detector: trigger_detector.inspect)
               end
             end
           end
