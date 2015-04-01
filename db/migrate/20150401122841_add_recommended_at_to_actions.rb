a566a0db807645640a6aba459c8500b6cb330a5a
cleverapollo <cleverapollo@gmail.com>
2015-04-01 16:15:15 +0300

    Migration fix

--- db/migrate/20150401122841_add_recommended_at_to_actions.rb
+++ db/migrate/20150401122841_add_recommended_at_to_actions.rb
@@ -1,9 +1,5 @@
 class AddRecommendedAtToActions < ActiveRecord::Migration
   def change
     add_column :actions, :recommended_at, :timestamp
-
-    Action.reset_column_information
-
-    Action.where('view_date >= ?', 2.weeks.ago).where('recommended_by IS NOT NULL').update_all(recommended_at: Time.current)
   end
 end
