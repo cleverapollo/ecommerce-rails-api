a1219d20c95be503fd0380ee6260f95fe16804c9
cleverapollo <cleverapollo@gmail.com>
2015-04-23 16:02:52 +0300

    Ignore some shops in migration --skip-ci

--- db/migrate/20150422100907_migrate_locations_to_locations_data.rb
+++ db/migrate/20150422100907_migrate_locations_to_locations_data.rb
@@ -1,6 +1,6 @@
 class MigrateLocationsToLocationsData < ActiveRecord::Migration
   def change
-    Item.where("locations != '{}'").where('shop_id NOT IN (?)', [114, 146]).find_each do |item|
+    Item.where("locations != '{}'").where('shop_id NOT IN (?)', [114, 146, 560, 670]).find_each do |item|
       item.locations.each do |location|
         item.locations_data[location] = {}
       end
