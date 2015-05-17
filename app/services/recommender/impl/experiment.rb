de177af8d2ad50671f57461d87efceb720ac9bde
cleverapollo <cleverapollo@gmail.com>
2015-05-17 10:31:34 +0300

    Limit for Experiment

--- app/services/recommender/impl/experiment.rb
+++ app/services/recommender/impl/experiment.rb
@@ -43,7 +43,7 @@ module Recommender
         end
 
         # Находим отсортированные товары
-        result = relation.where('sales_rate is not null and sales_rate > 0').order(sales_rate: :desc).limit(LIMIT).pluck(:id)
+        result = relation.where('sales_rate is not null and sales_rate > 0').order(sales_rate: :desc).limit(limit).pluck(:id)
 
         unless shop.strict_recommendations?
           # Если товаров недостаточно - рандом
