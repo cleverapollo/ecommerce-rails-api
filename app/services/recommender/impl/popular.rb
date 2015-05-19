b746f70a6569b918d128fc46b7b7ee675e26d491
cleverapollo <cleverapollo@gmail.com>
2015-05-19 18:28:05 +0300

    inject promo to popular

--- app/services/recommender/impl/popular.rb
+++ app/services/recommender/impl/popular.rb
@@ -56,8 +56,19 @@ module Recommender
                    result
                  end
 
-        # Вернем только id товаров
-        result.to_h.keys
+        # оставим только id товаров
+        result = result.to_h.keys
+
+
+        if result.size < params.limit && !shop.strict_recommendations?
+          # Если товаров недостаточно - рандом
+          result = inject_random_items(result)
+        end
+
+        # Добавляем продвижение брендов
+        result = inject_promotions(result)
+
+        result
       end
 
       def items_to_recommend
