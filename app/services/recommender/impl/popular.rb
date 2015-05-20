fd4e35c7a686a9adbfa4e3b54c5e4336729bf880
cleverapollo <cleverapollo@gmail.com>
2015-05-20 16:57:08 +0300

    experiment >> popular

--- app/services/recommender/impl/popular.rb
+++ app/services/recommender/impl/popular.rb
@@ -1,30 +1,9 @@
 module Recommender
   module Impl
-    class Popular < Recommender::Base
-
-      include ItemInjector
-      include CfHelper
-
-      LIMIT = 20
+    class Popular < Recommender::Personalized
 
       K_SR = 1.0
       K_CF = 1.0
-
-      def items_to_recommend
-        if shop.sectoral_algorythms_available?
-          result = super
-          if shop.category.wear?
-            gender = SectoralAlgorythms::Wear::Gender.calculate_for(user, shop: shop)
-            result = result.by_ca(gender: gender)
-            # TODO: фильтрация по размерам одежды
-          end
-          result
-
-        else
-          super
-        end
-      end
-
 
       def items_to_weight
         # Разные запросы в зависимости от присутствия или отсутствия категории
@@ -46,37 +25,12 @@ module Recommender
 
 
       # @return Int[]
-      def recommended_ids
-        # получим товары для взвешивания
-        i_w = items_to_weight
-
-        # Взвешиваем махаутом
-        cf_weighted = cf_weight(i_w)
-
+      def rescore(i_w, cf_weighted)
         # Взвешиваем по SR
-        sr_weighted = sr_weight(i_w)
-
-        # Рассчитываем финальную оценку
-        result = sr_weighted.merge(cf_weighted) do |key, sr, cf|
+        sr_weight(i_w).merge(cf_weighted) do |key, sr, cf|
+          # подмешиваем оценку SR
           (K_SR*sr.to_f + K_CF*cf.to_f)/(K_CF+K_SR)
-        end.sort do |x, y|
-          # сортируем по вычисленной оценке
-          x= x[1].to_i
-          y= y[1].to_i
-          y<=>x
         end
-
-        # Ограничиваем размер вывода
-        result = if result.size > params.limit
-                   result.take(params.limit)
-                 else
-                   result
-                 end
-
-        # оставим только id товаров
-        result = result.to_h.keys
-
-        inject_items(result)
       end
 
 
