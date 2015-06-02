7eaea8a6f350721e4437ef55d00be204050144a0
cleverapollo <cleverapollo@gmail.com>
2015-06-02 11:58:25 +0300

    REES-1487 Убрать индекс categories_sales_rate

--- db/migrate/20150602084248_drop_categories_sales_rate_index.rb
+++ db/migrate/20150602084248_drop_categories_sales_rate_index.rb
@@ -1,5 +1,5 @@
 class DropCategoriesSalesRateIndex < ActiveRecord::Migration
   def change
-    execute "DROP INDEX categories_sales_rate;"
+    remove_index :items, name: :categories_sales_rate
   end
 end
