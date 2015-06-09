6be6ae59f5535ff9c66ecd30822126f814df91e0
cleverapollo <cleverapollo@gmail.com>
2015-06-09 16:08:53 +0300

    Less code for tracking

--- app/services/brand_logger.rb
+++ app/services/brand_logger.rb
@@ -7,22 +7,16 @@ class BrandLogger
     # Либо создает новую запись, если не было за сегодня, либо обновляет существующую
     # @param advertiser_id Integer
     def track_view(advertiser_id)
-      if (row = AdvertiserStatistic.where(advertiser_id: advertiser_id).where(date: Date.current).first)
-        row.update views: (row.views + 1)
-      else
-        AdvertiserStatistic.create advertiser_id: advertiser_id, views: 1, date: Date.current
-      end
+      row = AdvertiserStatistic.find_or_create_by advertiser_id: advertiser_id, date: Date.current
+      row.update views: (row.views + 1)
     end
 
 
     # Трекает количество кликов продвигаемого товара
     # @param advertiser_id Integer
     def track_click(advertiser_id)
-      if (row = AdvertiserStatistic.where(advertiser_id: advertiser_id).where(date: Date.current).first)
-        row.update clicks: (row.clicks + 1)
-      else
-        AdvertiserStatistic.create advertiser_id: advertiser_id, clicks: 1, date: Date.current
-      end
+      row = AdvertiserStatistic.find_or_create_by advertiser_id: advertiser_id, date: Date.current
+      row.update clicks: (row.clicks + 1)
     end
 
 
@@ -30,18 +24,11 @@ class BrandLogger
     # @param advertiser_id Integer
     # @param recommended Boolean
     def track_purchase(advertiser_id, recommended = false)
-      if (row = AdvertiserStatistic.where(advertiser_id: advertiser_id).where(date: Date.current).first)
-        if recommended
-          row.update recommended_purchases: (row.recommended_purchases + 1)
-        else
-          row.update original_purchases: (row.original_purchases + 1)
-        end
+      row = AdvertiserStatistic.find_or_create_by advertiser_id: advertiser_id, date: Date.current
+      if recommended
+        row.update recommended_purchases: (row.recommended_purchases + 1)
       else
-        if recommended
-          AdvertiserStatistic.create advertiser_id: advertiser_id, recommended_purchases: 1, date: Date.current
-        else
-          AdvertiserStatistic.create advertiser_id: advertiser_id, original_purchases: 1, date: Date.current
-        end
+        row.update original_purchases: (row.original_purchases + 1)
       end
     end
 
