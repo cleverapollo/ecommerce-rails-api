0c683576aa2c3a5ab2896d50d594cbf6cc400975
cleverapollo <cleverapollo@gmail.com>
2015-06-11 22:09:28 +0300

    Избегаем дубликатов в статистике

--- app/services/brand_logger.rb
+++ app/services/brand_logger.rb
@@ -8,7 +8,7 @@ class BrandLogger
     # @param advertiser_id Integer
     # @param shop_id Integer
     def track_view(advertiser_id, shop_id)
-      row = AdvertiserStatistic.find_or_create_by advertiser_id: advertiser_id, date: Date.current
+      row = get_statistics_row advertiser_id, Date.current
       row.update views: (row.views + 1)
       AdvertiserShop.where(advertiser_id: advertiser_id).where(shop_id: shop_id).update_all last_event_at: Time.current
     end
@@ -17,7 +17,7 @@ class BrandLogger
     # Трекает количество кликов продвигаемого товара
     # @param advertiser_id Integer
     def track_click(advertiser_id, recommended = false)
-      row = AdvertiserStatistic.find_or_create_by advertiser_id: advertiser_id, date: Date.current
+      row = get_statistics_row advertiser_id, Date.current
       if recommended
         row.update recommended_clicks: (row.recommended_clicks + 1)
       else
@@ -30,13 +30,28 @@ class BrandLogger
     # @param advertiser_id Integer
     # @param recommended Boolean
     def track_purchase(advertiser_id, recommended = false)
-      row = AdvertiserStatistic.find_or_create_by advertiser_id: advertiser_id, date: Date.current
+      row = get_statistics_row advertiser_id, Date.current
       if recommended
         row.update recommended_purchases: (row.recommended_purchases + 1)
       else
         row.update original_purchases: (row.original_purchases + 1)
       end
     end
+
+    # Создает или находит запись о статистике рекламодателя за дату
+    # Уникальный индекс, и чтобы не было дубликатов, кидаем ошибку в случае конкуретного создания записей, после чего находим уже созданную запись
+    # @param advertiser_id [Integer]
+    # @param date [Date]
+    # @return AdvertiserStatistic
+    def get_statistics_row(advertiser_id, date)
+      begin
+        row = AdvertiserStatistic.find_or_create_by! advertiser_id: advertiser_id, date: date
+      rescue
+        row = AdvertiserStatistic.find_by advertiser_id: advertiser_id, date: date
+      end
+      row
+    end
+
 
   end
 
