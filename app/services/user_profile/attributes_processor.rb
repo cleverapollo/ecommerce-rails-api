8595786144a85b9bf4c877849b293178bec8914e
cleverapollo <cleverapollo@gmail.com>
2015-06-15 16:34:29 +0300

    REES-1524 Проверить JS SDK push_attributes + тесты

--- app/services/user_profile/attributes_processor.rb
+++ app/services/user_profile/attributes_processor.rb
@@ -5,11 +5,24 @@ module UserProfile
   class AttributesProcessor
     def self.process(shop, user, attributes)
       attributes.stringify_keys!
+
       exists = false
       user.profile_attributes.where(shop: shop).find_each do |profile_attribute|
         exists = true if profile_attribute.value == attributes
       end
       user.profile_attributes.create!(shop: shop, value: attributes) unless exists
+
+      # Проверяем на наличие email
+      if attributes['email'].present?
+        # И сохраняем
+        client = shop.clients.find_or_create_by!(user_id: user.id)
+
+        if email = IncomingDataTranslator.email(attributes['email'])
+          client.email = email
+        end
+        client.save
+      end
+
     end
   end
 end
--- spec/services/user_profile/attributes_processor_spec.rb
+++ spec/services/user_profile/attributes_processor_spec.rb
@@ -4,21 +4,27 @@ describe UserProfile::AttributesProcessor do
   describe '.process' do
     let!(:shop) { create(:shop) }
     let!(:user) { create(:user) }
-    let!(:attributes) { { 'gender' => 'f', 'type' => 'shoe', 'size' => 'e44' } }
+    let!(:attributes) { { 'gender' => 'f', 'type' => 'shoe', 'size' => 'e44', 'email' => 'test@example.com' } }
 
     subject { UserProfile::AttributesProcessor.process(shop, user, attributes) }
 
+    it 'set correct email' do
+      subject
+      expect { user.clients.first.email = 'test@example.com' }
+    end
+
     context 'when profile attribute is new' do
       it 'stores profile attribute' do
-        expect{ subject }.to change { ProfileAttribute.count }.from(0).to(1)
+        expect { subject }.to change { ProfileAttribute.count }.from(0).to(1)
       end
     end
 
     context 'when profile attribute already logged' do
       before { ProfileAttribute.create(user: user, shop: shop, value: attributes) }
       it 'does nothing' do
-        expect{ subject }.to_not change { ProfileAttribute.count }
+        expect { subject }.to_not change { ProfileAttribute.count }
       end
+
     end
   end
 end
