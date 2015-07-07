7e987a49e854745aa334a69359df180bad3a0578
cleverapollo <cleverapollo@gmail.com>
2015-07-07 16:36:23 +0300

    New server config

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,18 +48,19 @@ namespace :deploy do
   desc 'Stop unicorn'
   task :stop do
     on roles(:app), in: :sequence, wait: 5 do
-      execute "kill -s QUIT `cat #{shared_path}/pids/unicorn.pid`"
+      execute "kill -s QUIT `cat #{shared_path}/tmp/pids/unicorn.pid`"
     end
   end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute "kill -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
+      execute "kill -s USR2 `cat #{shared_path}/tmp/pids/unicorn.pid`"
     end
   end
 
 end
+
 
 
 
