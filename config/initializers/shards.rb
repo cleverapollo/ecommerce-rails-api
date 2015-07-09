dbf27c9f0ef14c3b27caa6a449dbf1d5b7ec1fd2
cleverapollo <cleverapollo@gmail.com>
2015-07-09 13:41:08 +0300

    Убрал лишние базы в shards.yml

--- config/initializers/shards.rb
+++ config/initializers/shards.rb
@@ -1,4 +1,2 @@
 db_conf = YAML::load(File.open(File.join(Rails.root,'config','shards.yml')))
 MASTER_DB = db_conf[Rails.env]['master']
-API_DB_00 = db_conf[Rails.env]['api_db_00']
-API_DB_01 = db_conf[Rails.env]['api_db_01']
