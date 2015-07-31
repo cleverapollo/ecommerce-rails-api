a5d4440191608583808bdf687bd7dd21479a09ee
cleverapollo <cleverapollo@gmail.com>
2015-07-31 14:22:14 +0300

    REES-1711 Сделать так, чтобы при замене логотипа в рассылках не приходилось менять шаблоны

--- app/models/mailings_settings.rb
+++ app/models/mailings_settings.rb
@@ -8,7 +8,15 @@ class MailingsSettings < ActiveRecord::Base
   validates :shop, presence: true
   validates :send_from, presence: true
 
+  has_attached_file :logo, styles: { original: '500x500>', main: '170>x', medium: '130>x', small: '100>x' }
+  validates_attachment_content_type :logo, content_type: /\Aimage/
+  validates_attachment_file_name :logo, matches: [/png\Z/i, /jpe?g\Z/i]
+
   def enabled?
     !shop.restricted?
+  end
+
+  def fetch_logo_url
+    self.logo.present? ? URI.join("http://#{ActionController::Base.asset_host}", self.logo.url).to_s : ''
   end
 end
