0fd255fdbfb7190a0f566a5cca2a80e818c300cb
cleverapollo <cleverapollo@gmail.com>
2015-08-31 14:46:57 +0300

    Коммит для проверки rollbar 807

--- app/services/trigger_mailings/triggers/recently_purchased.rb
+++ app/services/trigger_mailings/triggers/recently_purchased.rb
@@ -5,6 +5,7 @@ module TriggerMailings
         time_range = (7.day.ago.beginning_of_day)..(7.day.ago.end_of_day)
         # Находим покупки, которые были сделаны 7 дней назад
         orders_relation = user.orders.where(shop: shop).where(date: time_range).order(date: :desc)
+
         orders_relation = orders_relation.successful if shop.track_order_status?
 
         orders_relation.each do |order|
