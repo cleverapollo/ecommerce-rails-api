451719952485c875c5e32b2fffce378fc722ed99
cleverapollo <cleverapollo@gmail.com>
2015-09-17 14:12:45 +0300

    check last_try_to_load_yml_at in yml worker

--- db/tests_schema.rb
+++ db/tests_schema.rb
@@ -516,6 +516,7 @@ ActiveRecord::Schema.define(version: 20150729145844) do
     t.integer  "yml_errors",                                                        default: 0,     null: false
     t.integer  "trigger_pause",                                                     default: 14
     t.integer  "yml_load_period",                                                   default: 24,    null: false
+    t.datetime "last_try_to_load_yml_at"
   end
 
   add_index "shops", ["cms_id"], name: "index_shops_on_cms_id", using: :btree
