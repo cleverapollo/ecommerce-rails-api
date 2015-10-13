cb6309b9bb7c4cbd2326c93727776c9aa71e6d63
cleverapollo <cleverapollo@gmail.com>
2015-10-13 13:09:21 +0300

    Удалил старые модели

--- app/models/advertiser.rb
+++ app/models/advertiser.rb
@@ -5,13 +5,6 @@ class Advertiser < MasterTable
 
   has_many :brand_campaigns
 
-  scope :active, -> { where(campaign_launched: true).where('balance > 0') }
-  scope :prioritized, -> { order(priority: :desc) }
-  scope :expansion, -> { where(is_expansion: true)}
-
-
-
-
   def first_in_selection(item_ids)
     Item.where(id:item_ids, brand:downcase_brand).where.not(brand:nil).by_sales_rate.limit(1)[0].try(:id)
   end
@@ -30,11 +23,6 @@ class Advertiser < MasterTable
     Item.where(shop_id:shop_id, brand:downcase_brand).where.not(id:excluded_ids, brand:nil)
         .by_sales_rate.limit(1)[0].try(:id)
     
-  end
-
-  # Активна ли рекламная кампания?
-  def active?
-    campaign_launched? && balance > 0
   end
 
 end
