a7cd0c0445dcaaeb37285358a72fcafd04322992
cleverapollo <cleverapollo@gmail.com>
2015-10-15 16:09:29 +0300

    Склейка пользователей по пушу

--- app/services/user_fetcher.rb
+++ app/services/user_fetcher.rb
@@ -34,21 +34,6 @@ class UserFetcher
       client.update(location: location)
     end
 
-    if email.present?
-      client_email = @email
-      # Найдем пользователя с тем же мылом в данном магазине
-      if client_with_current_mail = shop.clients.where.not(id: client.id).find_by(email: client_email)
-        old_user = client_with_current_mail.user
-        client_with_current_mail.each { |merge_client| UserMerger.merge(old_user, merge_client.user) unless merge_client.user.id==old_user.id }
-      else
-        # И при этом этого мыла больше нигде нет
-        # Запоминаем его для текущего пользователя
-        # Адовый способ не ломать транзакцию
-        exclude_query = "NOT EXISTS (SELECT 1 FROM clients WHERE shop_id = #{shop.id} and email = '#{client_email}')"
-        shop.clients.where(id: client.id).where(exclude_query).update_all(email: client_email)
-      end
-    end
-
     # Если известен ID пользователя в магазине
     if external_id.present?
       if old_client = shop.clients.where.not(id: client.id).find_by(external_id: external_id)
@@ -63,6 +48,10 @@ class UserFetcher
         exclude_query = "NOT EXISTS (SELECT 1 FROM clients WHERE shop_id = #{shop.id} and external_id = '#{external_id}')"
         shop.clients.where(id: client.id).where(exclude_query).update_all(external_id: external_id)
       end
+    end
+
+    if email.present?
+      UserMerger.merge_by_mail(shop, client, email)
     end
 
     result
--- app/services/user_profile/attributes_processor.rb
+++ app/services/user_profile/attributes_processor.rb
@@ -17,21 +17,7 @@ module UserProfile
         if email = IncomingDataTranslator.email(attributes['email'])
           # И сохраняем
           client = shop.clients.find_or_create_by!(user_id: user.id)
-          if email.present?
-            client_email = @email
-            # Найдем пользователя с тем же мылом в данном магазине
-            if client_with_current_mail = shop.clients.where.not(id: client.id).find_by(email: client_email)
-              old_user = client_with_current_mail.user
-              client_with_current_mail.each { |merge_client| UserMerger.merge(old_user, merge_client.user) unless merge_client.user.id==old_user.id }
-            else
-              # И при этом этого мыла больше нигде нет
-              # Запоминаем его для текущего пользователя
-              # Адовый способ не ломать транзакцию
-              exclude_query = "NOT EXISTS (SELECT 1 FROM clients WHERE shop_id = #{shop.id} and email = '#{client_email}')"
-              shop.clients.where(id: client.id).where(exclude_query).update_all(email: client_email)
-            end
-          end
-          
+          UserMerger.merge_by_mail(shop, client, email)
         end
       end
 
