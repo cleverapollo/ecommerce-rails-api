895db87e640929dcb3a0d9e3d86912041d4abc07
cleverapollo <cleverapollo@gmail.com>
2015-11-05 12:35:24 +0300

    SQL injection fixed

--- app/services/user_fetcher.rb
+++ app/services/user_fetcher.rb
@@ -49,8 +49,8 @@ class UserFetcher
         # И при этом этого ID больше нигде нет
         # Запоминаем его для текущего пользователя
         # Адовый способ не ломать транзакцию
-        exclude_query = "NOT EXISTS (SELECT 1 FROM clients WHERE shop_id = #{shop.id} and external_id = '#{external_id}')"
-        shop.clients.where(id: client.id).where(exclude_query).update_all(external_id: external_id)
+        exclude_query = "NOT EXISTS (SELECT 1 FROM clients WHERE shop_id = ? and external_id = ?)"
+        shop.clients.where(id: client.id).where(exclude_query, shop.id, external_id).update_all(external_id: external_id)
         user = client.user
       end
     end
