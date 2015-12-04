8cfb4edb6f9976d6214f9ee62e859b7f93f68403
cleverapollo <cleverapollo@gmail.com>
2015-12-04 13:55:58 +0300

    Add staging environment

---
+++ config/deploy/staging.rb
@@ -0,0 +1,18 @@
+role :app, %w{5.101.116.131}
+role :web, %w{5.101.116.131}
+role :db,  %w{5.101.116.131}
+
+set :stage, :stage
+set :shard, :stage
+
+set :log_level, :info
+
+set :ssh_options, {
+  user: 'rails',
+  forward_agent: true,
+  port: 21212
+}
+
+set :deploy_to, "/home/rails/staging.rees46.com"
+set :branch, 'develop'
+set :rails_env, 'production'
