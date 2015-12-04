9054f0d22d56eb57729b6410b71e284a69d27f06
cleverapollo <cleverapollo@gmail.com>
2015-12-04 14:07:09 +0300

    Change config for staging environment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
 # In the future make redis database equal shard id
 redis_db = [0,0,2][SHARD_ID.to_i]
 
-Redis.current ||= Redis.new({ url: "redis://localhost:6379/#{redis_db}", namespace: "rees46_api_#{Rails.env}" })
+if Rails.env.staging?
+  Redis.current ||= Redis.new({ url: "redis://localhost:6379/7", namespace: "rees46_api_#{Rails.env}" })
+else
+  Redis.current ||= Redis.new({ url: "redis://localhost:6379/#{redis_db}", namespace: "rees46_api_#{Rails.env}" })
+end
 
 Sidekiq.configure_server do |config|
   config.redis = Redis.current
