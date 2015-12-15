08831cce4f022f919824365edbbd3c008e2503b1
cleverapollo <cleverapollo@gmail.com>
2015-12-15 11:53:31 +0300

    fix digest mailing email worker spec

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -3,12 +3,9 @@ FactoryGirl.define do
     uniqid
     price 100
     categories '{5}'
-    is_available true
-    ignored false
-    url 'http://example.com/item/123'
+    url ''# 'http://example.com/item/123'
     image_url 'http://example.com/item/123.jpg'
     name 'test'
-    widgetable true
     description ''
 
     trait :widgetable do
