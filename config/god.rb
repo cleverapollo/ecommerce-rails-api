8b9ade78d333520d15770765dc5dc1d3b26c8737
cleverapollo <cleverapollo@gmail.com>
2016-02-12 12:53:42 +0300

    Глобальный конфиг GOD

--- config/god.rb
+++ config/god.rb
@@ -37,9 +37,9 @@ end
 God.watch do |w|
   ROOT = '/home/rails/rees46_cf_daemons/current'
   w.name = 'cf_events_saver'
-  w.start = "ruby #{ROOT}/bin/cf_events_saver.rb"
+  w.start = "RAILS_ENV=production ruby #{ROOT}/bin/cf_events_saver.rb"
   w.keepalive interval: 10.seconds
-  w.env = {
-      'RAILS_ENV' => 'production',
-  }
+  # w.env = {
+  #     'RAILS_ENV' => 'production',
+  # }
 end
