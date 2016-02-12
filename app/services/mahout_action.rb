b4d92b4ab8373419f13d829c63faa62b7858b313
cleverapollo <cleverapollo@gmail.com>
2016-02-12 16:25:40 +0300

    Перелинковка данных в Mahout тоже через Rabbit.

--- app/services/mahout_action.rb
+++ app/services/mahout_action.rb
@@ -3,12 +3,36 @@
 #
 class MahoutAction
 
+  MAHOUT_QUEUE_RELINK_USER = 'rees46.cf.relink_user'
+
   class << self
+
+    def publish_to_mahout(message)
+      q  = channel_to_mahout.queue MAHOUT_QUEUE_RELINK_USER
+      x  = channel_to_mahout.default_exchange
+      x.publish message, :routing_key => q.name
+    end
+
+    def connection_to_mahout
+      @connection_to_mahout ||= Bunny.new.tap do |c|
+        c.start
+      end
+    end
+
+    def channel_to_mahout
+      @channel_to_mahout ||= connection_to_mahout.create_channel
+    end
+
+
+
     def relink_user(options = {})
-      mahout_service = MahoutService.new
-      mahout_service.open
-      mahout_service.relink_user(options.fetch(:from).id, options.fetch(:to).id)
-      mahout_service.close
+
+      MahoutAction.publish_to_mahout [options.fetch(:from).id, options.fetch(:to).id].join(',')
+
+      # mahout_service = MahoutService.new
+      # mahout_service.open
+      # mahout_service.relink_user(options.fetch(:from).id, options.fetch(:to).id)
+      # mahout_service.close
     end
   end
 
