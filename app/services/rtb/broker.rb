03bf461791a4a9c5156320c01980c1dfdb741743
cleverapollo <cleverapollo@gmail.com>
2016-03-07 01:54:37 +0300

    Additional data for rtb jobs.

--- app/services/rtb/broker.rb
+++ app/services/rtb/broker.rb
@@ -17,7 +17,7 @@ module Rtb
         # Комиссия с продажи такого товара – 92 рубля, вроде годная цена
         if item.price >= 2000 && item.widgetable? && item.is_available? && item.name.length < 255 && item.image_url.length < 255 && item.url.length < 255
           if rtb_item = RtbJob.find_by(shop_id: shop.id, user_id: user.id, item_id: item.id)
-            rtb_item.update counter: 0, date: Date.current, price: item.price
+            rtb_item.update counter: 0, date: Date.current, price: item.price, image: item.image_url, name: item.name, currency: shop.currency, url: item.url
           else
             RtbJob.create! shop_id: shop.id, user_id: user.id, item_id: item.id, date: Date.current, counter: 0, price: item.price, image: item.image_url, name: item.name, currency: shop.currency, url: item.url
           end
