ee95d5bd150bf5e50cb5c92d1f758b79bbd01533
cleverapollo <cleverapollo@gmail.com>
2016-03-23 17:14:58 +0300

    REES-2305 Перерасчет баланса при синхронизации заказов

---
+++ app/services/cpa_report.rb
@@ -0,0 +1,24 @@
+class CpaReport
+
+  class << self
+
+    def fee(order, shop)
+      v = 0
+      if order.source_type == 'RtbImpression'
+        v = order.value * shop.triggers_cpa / 100.0
+        v = shop.triggers_cpa_cap if v > shop.triggers_cpa_cap
+      end
+      if order.source_type == 'TriggerMail'
+        v = order.value * shop.triggers_cpa / 100.0
+        v = shop.triggers_cpa_cap if v > shop.triggers_cpa_cap
+      end
+      if order.source_type == 'DigestMail'
+        v = order.value * shop.digests_cpa / 100.0
+        v = shop.digests_cpa_cap if v > shop.digests_cpa_cap
+      end
+      v
+    end
+
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315101036) do
+ActiveRecord::Schema.define(version: 20160323140209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -319,6 +319,7 @@ ActiveRecord::Schema.define(version: 20160315101036) do
     t.string   "source_type"
     t.integer  "status",                        default: 0,     null: false
     t.date     "status_date"
+    t.boolean  "compensated"
   end
 
   add_index "orders", ["date"], name: "index_orders_on_date", using: :btree
