8853f75e2e7add5d137882a497c1a385ab317689
cleverapollo <cleverapollo@gmail.com>
2016-04-12 16:26:33 +0300

    God config for new java

--- config/god.rb
+++ config/god.rb
@@ -1,54 +1,54 @@
 
 
-God.watch do |w|
-
-  ROOT = '/home/rails/rees46_brb/current'
-
-  pid_file = "#{ROOT}/tmp/pids/server.pid"
-  rvm_command = "rvm jruby-1.7.20 do bundle exec"
-  pid_command = "ps aux | grep 'jruby' | grep -v grep | awk '{print $2}' | head -n1"
-
-  w.name = 'brb'
-  w.dir = ROOT
-
-  w.env = {
-      'RAILS_ENV' => 'production',
-      'JRUBY_OPTS' => '-J-Xmx1512m -J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC -J-Djruby.jit.threshold=0',
-      'MAHOUT_DIR' => '/home/rails/rees46_brb/shared/libs/mahout'
-  }
-
-  # sleep для того, чтобы сервер успел прогрузиться (иначе будет кривой pid)
-  w.start = "cd #{ROOT} && #{rvm_command} bin/server.rb &> #{ROOT}/log/server.txt && sleep 5 && echo $(#{pid_command}) > #{pid_file}"
-  w.stop = "kill $(ps aux | grep 'jruby'| grep -v grep | awk '{print $2}')"
-
-  w.pid_file = pid_file
-  w.start_grace = 10.seconds
-
-  #w.behavior(:clean_pid_file)
-
-  w.log = "#{ROOT}/log/server_god.log"
-  w.keepalive(:interval=>10.seconds)
-
-end
-
-
+# God.watch do |w|
+#
+#   ROOT = '/home/rails/rees46_brb/current'
+#
+#   pid_file = "#{ROOT}/tmp/pids/server.pid"
+#   rvm_command = "rvm jruby-1.7.20 do bundle exec"
+#   pid_command = "ps aux | grep 'jruby' | grep -v grep | awk '{print $2}' | head -n1"
+#
+#   w.name = 'brb'
+#   w.dir = ROOT
+#
+#   w.env = {
+#       'RAILS_ENV' => 'production',
+#       'JRUBY_OPTS' => '-J-Xmx1512m -J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC -J-Djruby.jit.threshold=0',
+#       'MAHOUT_DIR' => '/home/rails/rees46_brb/shared/libs/mahout'
+#   }
+#
+#   # sleep для того, чтобы сервер успел прогрузиться (иначе будет кривой pid)
+#   w.start = "cd #{ROOT} && #{rvm_command} bin/server.rb &> #{ROOT}/log/server.txt && sleep 5 && echo $(#{pid_command}) > #{pid_file}"
+#   w.stop = "kill $(ps aux | grep 'jruby'| grep -v grep | awk '{print $2}')"
+#
+#   w.pid_file = pid_file
+#   w.start_grace = 10.seconds
+#
+#   #w.behavior(:clean_pid_file)
+#
+#   w.log = "#{ROOT}/log/server_god.log"
+#   w.keepalive(:interval=>10.seconds)
+#
+# end
 
 
-God.watch do |w|
-  ROOT = '/home/rails/rees46_cf_daemons/current'
-  rvm_command = "rvm ruby-2.2.3 do bundle exec"
-  pid_file = "#{ROOT}/tmp/pids/saver.pid"
-  w.name = 'cf_events_saver'
-  w.start = "cd #{ROOT} && RAILS_ENV=production #{rvm_command} #{ROOT}/bin/cf_events_saver.rb"
-  w.stop = "kill -s QUIT $(cat #{pid_file})"
-  w.keepalive interval: 10.seconds
-  w.log = "/var/log/god/cf_events_saver.log"
-  w.env = {
-      'RAILS_ENV' => 'production',
-  }
-  w.pid_file = pid_file
-  w.behavior(:clean_pid_file)
-end
+
+
+# God.watch do |w|
+#   ROOT = '/home/rails/rees46_cf_daemons/current'
+#   rvm_command = "rvm ruby-2.2.3 do bundle exec"
+#   pid_file = "#{ROOT}/tmp/pids/saver.pid"
+#   w.name = 'cf_events_saver'
+#   w.start = "cd #{ROOT} && RAILS_ENV=production #{rvm_command} #{ROOT}/bin/cf_events_saver.rb"
+#   w.stop = "kill -s QUIT $(cat #{pid_file})"
+#   w.keepalive interval: 10.seconds
+#   w.log = "/var/log/god/cf_events_saver.log"
+#   w.env = {
+#       'RAILS_ENV' => 'production',
+#   }
+#   w.pid_file = pid_file
+#   w.behavior(:clean_pid_file)
+# end
 
 
 
@@ -67,3 +67,18 @@ God.watch do |w|
   w.pid_file = pid_file
   w.behavior(:clean_pid_file)
 end
+
+
+
+God.watch do |w|
+  ROOT = '/home/rails/rees46_recommendations/'
+  w.name = 'cf'
+  w.start = "/home/rails/rees46_recommendations/init.d start"
+  w.stop = "/home/rails/rees46_recommendations/init.d stop"
+  w.keepalive interval: 10.seconds
+  w.log = "/home/rails/rees46_recommendations/log/god.log"
+  w.env = {}
+  w.pid_file = "#{ROOT}/tmp/pids/relink.pid"
+  w.behavior(:clean_pid_file)
+end
+
