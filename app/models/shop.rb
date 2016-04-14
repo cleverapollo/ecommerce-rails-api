28349a74b2591b6cf87656f354950ab4fd758628
cleverapollo <cleverapollo@gmail.com>
2016-04-15 00:17:22 +0300

    REES-2356 Не фиксировать ошибку обработки YML в случае остановки Sidekiq

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -131,6 +131,8 @@ class Shop < MasterTable
       Rollbar.warning(e, "Ошибка синтаксиса YML", shop_id: id)
       ErrorsMailer.yml_syntax_error(self, 'В YML-файле встречаются товары с одинаковыми идентификаторами. Каждое товарное предложение (оффер) должно содержать уникальный идентификатор товара, не повторяющийся в пределах одного YML-файла.').deliver_now
       increment!(:yml_errors)
+    rescue Sidekiq::Shutdown => e
+      Rollbar.info(e, "Sidekiq shutdown, abort YML processing", shop_id: id)
     rescue Exception => e
       ErrorsMailer.yml_import_error(self, e).deliver_now
       Rollbar.warning(e, "YML process error", shop_id: id)
