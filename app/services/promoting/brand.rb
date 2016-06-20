3d0792ddfe89153ef13d2f5a8a5054d83c96b2e3
cleverapollo <cleverapollo@gmail.com>
2016-06-20 17:15:17 +0300

    Поддержка рекламных кампаний без учета категорий

--- app/services/promoting/brand.rb
+++ app/services/promoting/brand.rb
@@ -22,7 +22,7 @@ module Promoting
       # Используется только в рекомендере Similar
       # @param item [Item]
       # @param expansion_only [Boolean]
-      # @return BrandCampaign[]
+      # @return BrandCampaign::ActiveRecord_Relation
       def brand_campaign_for_item(item, expansion_only)
 
         # Если бренд не определен, то ничего нет
@@ -35,11 +35,16 @@ module Promoting
 
 
 
-
+      # Возвращает список рекламных кампаний бренда, соответствующих категориям или настроенным на любые категории
+      # @param shop_id [Integer]
+      # @param categories [String[]] Массив идентификаторов категорий со стороны магазина
+      # @param expansion_only [Boolean]
+      # @return BrandCampaign::ActiveRecord_Relation
       def brand_campaigns_for_categories(shop_id, categories, expansion_only)
         relation = BrandCampaign.active.prioritized
         relation = relation.expansion if expansion_only
-        relation.where(id: BrandCampaignItemCategory.where( item_category_id: ItemCategory.where(shop_id: shop_id,external_id: categories).pluck(:id) ).select('brand_campaign_id') )
+        ids_by_categories = BrandCampaignItemCategory.where( item_category_id: ItemCategory.where(shop_id: shop_id, external_id: categories).pluck(:id) ).pluck(:brand_campaign_id).uniq
+        relation.where('in_all_categories IS TRUE OR id IN (?)', ids_by_categories)
       end
 
       def brand_campaigns_for_shop(shop, expansion_only)
