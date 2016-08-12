81f6982d156ea0de444a182f9d7a3347b6317204
cleverapollo <cleverapollo@gmail.com>
2016-08-12 11:25:47 +0300

    Fixed scope for second abandoned cart web push

--- app/models/client.rb
+++ app/models/client.rb
@@ -27,8 +27,8 @@ class Client < ActiveRecord::Base
   end
 
   scope :ready_for_second_abandoned_cart_web_push, -> (shop) do
-    web_push_trigger = WebPushTrigger.where(shop: shop).where(trigger_type: 'abandoned_cart').select(:id)
-    clients_ids = WebPushTriggerMessage.where(shop: shop).where(created_at: 28.hours.ago..24.hours.ago).where(clicked: false).where(web_push_trigger_id: web_push_trigger.id).select(:id)
+    web_push_trigger_id = WebPushTrigger.where(shop: shop).where(trigger_type: 'abandoned_cart').limit(1).pluck(:id).first
+    clients_ids = WebPushTriggerMessage.where(shop: shop).where(created_at: 28.hours.ago..24.hours.ago).where(clicked: false).where(web_push_trigger_id: web_push_trigger_id).select(:id)
     where(id: clients_ids).where(last_web_push_sent_at: 28.hours.ago..24.hours.ago)
   end
   scope :ready_for_web_push_trigger, -> (shop) { where("web_push_enabled IS TRUE AND ((last_web_push_sent_at is null) OR last_web_push_sent_at < ? )", shop.trigger_pause.days.ago) }
