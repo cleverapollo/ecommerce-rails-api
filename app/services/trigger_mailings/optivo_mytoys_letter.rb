93c5babbf3fd7c8376f6f5fcd40585f4c845843e
cleverapollo <cleverapollo@gmail.com>
2016-09-12 20:03:29 +0300

    Bug in IDs

--- app/services/trigger_mailings/optivo_mytoys_letter.rb
+++ app/services/trigger_mailings/optivo_mytoys_letter.rb
@@ -146,7 +146,7 @@ module TriggerMailings
         user_id: @client.user_id,
         shop_id: @shop.id,
         trigger_type: @trigger.code.underscore,
-        recommended_items: @trigger.recommended_ids(8),
+        recommended_items: @trigger.recommendations(8),
         source_items: [],
         email: client.email,
         trigger_mail_code: @trigger_mail.code
