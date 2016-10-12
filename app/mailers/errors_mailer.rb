c9ef969969d7f702b9d9e2a9a3b4ccd412e81265
cleverapollo <cleverapollo@gmail.com>
2016-10-12 10:02:24 +0300

    Prevent notice managers about orders import problem

--- app/mailers/errors_mailer.rb
+++ app/mailers/errors_mailer.rb
@@ -1,9 +1,9 @@
 class ErrorsMailer < ActionMailer::Base
-  default from: 'REES46 <mk@rees46.com>',
+  default from: 'REES46 <reports@rees46.com>',
           bcc: ['mk@rees46.com', 'av@rees46.com', 'dz@rees46.com']
 
   def orders_import_error(email, message, params)
-    mail(to: email, subject: "Ошибка при импорте заказов") do |format|
+    mail(to: email, bcc: [], subject: "Ошибка при импорте заказов") do |format|
       format.text { render text: "#{message}\n\n\n#{params.inspect}" }
       format.html { render text: "#{message}<hr/>#{params.inspect}" }
     end
