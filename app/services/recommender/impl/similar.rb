1633f5ca542f1333256b40038599ffd02d07e161
cleverapollo <cleverapollo@gmail.com>
2016-10-20 22:02:08 +0300

    hot fix similar recommender

--- app/services/recommender/impl/similar.rb
+++ app/services/recommender/impl/similar.rb
@@ -45,7 +45,7 @@ module Recommender
 
           # (auto_compatibility @> '[{"brand": "BMW"}]' OR auto_compatibility IS NULL)
           if item.auto_compatibility.present?
-            result = result.where("(auto_compatibility->'brands' ?| ARRAY[:brand] #{item.auto_compatibility['models'].present? ? "OR auto_compatibility->'models' ?| ARRAY[:model]" : ''})", brand: item.auto_compatibility['brand'], model: item.auto_compatibility['model'])
+            result = result.where("(auto_compatibility->'brands' ?| ARRAY[:brand] #{item.auto_compatibility['models'].present? ? "OR auto_compatibility->'models' ?| ARRAY[:model]" : ''})", brand: item.auto_compatibility['brands'], model: item.auto_compatibility['models'])
           end
 
           if item.auto_vds.present?
