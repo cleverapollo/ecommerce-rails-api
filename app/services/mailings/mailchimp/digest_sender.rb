e95271a7988c0172d3169cd5901ad0a3581d4b68
cleverapollo <cleverapollo@gmail.com>
2016-11-01 18:45:04 +0300

    Fix Rollbar #1755

--- app/services/mailings/mailchimp/digest_sender.rb
+++ app/services/mailings/mailchimp/digest_sender.rb
@@ -27,7 +27,7 @@ module Mailings
         digest_mailing.finish!
 
         member_fields_counter = api.get_list(digest_mailing.mailchimp_list_id, 'stats.merge_field_count')['stats']['merge_field_count']
-        merge_fields = api.get_merge_fields(digest_mailing.mailchimp_list_id, 14, 'merge_fields.merge_id,merge_fields.tag')['merge_fields']
+        merge_fields = api.get_merge_fields(digest_mailing.mailchimp_list_id, member_fields_counter, 'merge_fields.merge_id,merge_fields.tag')['merge_fields']
 
         merge_fields_tags = []
 
