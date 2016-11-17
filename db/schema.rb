299ab238f7501454b5d7e551fb0f5d5f12d3f1b9
cleverapollo <cleverapollo@gmail.com>
2016-11-17 09:54:06 +0300

    REES-2621 Дата включения/выключения триггера

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111144134) do
+ActiveRecord::Schema.define(version: 20161117063956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -544,6 +544,7 @@ ActiveRecord::Schema.define(version: 20161111144134) do
     t.integer  "image_width",                             default: 180
     t.integer  "image_height",                            default: 180
     t.string   "mailchimp_campaign_id"
+    t.datetime "activated_at"
   end
 
   add_index "trigger_mailings", ["shop_id", "trigger_type"], name: "index_trigger_mailings_on_shop_id_and_trigger_type", unique: true, using: :btree
