3fa801718ba48b45ab46d22e3339b5b65927d3c3
cleverapollo <cleverapollo@gmail.com>
2016-11-17 11:52:42 +0300

    Rtb job depends on min payment in customer's currency

--- app/services/rtb/broker.rb
+++ app/services/rtb/broker.rb
@@ -18,7 +18,7 @@ module Rtb
       popunder_job = nil
       items.each do |item|
         # Комиссия с продажи такого товара – 46 рублей
-        if item.price && item.price >= 1000 && item.widgetable? && item.is_available? && item.name.to_s.length < 255 && item.image_url.to_s.length < 255 && item.url.to_s.length < 255
+        if item.price && item.price >= @customer.currency.min_payment && item.widgetable? && item.is_available? && item.name.to_s.length < 255 && item.image_url.to_s.length < 255 && item.url.to_s.length < 255
           if rtb_item = RtbJob.find_by(shop_id: shop.id, user_id: user.id, item_id: item.id)
             rtb_item.update counter: 0, date: Date.current, price: item.price, image: item.image_url, name: item.name, currency: shop.currency, url: item.url
           else
