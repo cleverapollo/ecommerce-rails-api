cbf32921c4a3e9af0f6f46b663e5201d3f327338
cleverapollo <cleverapollo@gmail.com>
2016-12-14 19:03:06 +0300

    REES-3418 Не локализовано системное письмо про YML-файл (part 1)

--- app/mailers/errors_mailer.rb
+++ app/mailers/errors_mailer.rb
@@ -2,41 +2,61 @@ class ErrorsMailer < ActionMailer::Base
   default from: 'REES46 <reports@rees46.com>',
           bcc: ['mk@rees46.com', 'av@rees46.com', 'dz@rees46.com']
 
+  def yml_import_error(shop, reason)
+    @shop = shop
+    @reason = reason
+    I18n.locale = @shop.customer.language || 'en'
+    @manager = @shop.manager || Customer.default_manager(I18n.locale.to_s)
+
+    m = mail(from: @manager.email, to: @shop.customer.email, bcc: @manager.email, subject: I18n.t('errors_mailer.subject.yml_import_error'))
+    m.header['List-Id'] = "<notification errors_mailer:yml_import_error>"
+    m.header['Feedback-ID'] = "#{@shop.customer.email}:yml_import_error:errors_mailer:rees46mailer"
+    m
+  end
+
+  def yml_url_not_respond(shop)
+    @shop = shop
+    I18n.locale = @shop.customer.language || 'en'
+    @manager = @shop.manager || Customer.default_manager(I18n.locale.to_s)
+
+    m = mail(from: @manager.email, to: @shop.customer.email, bcc: @manager.email, subject: I18n.t('errors_mailer.subject.yml_url_not_respond'))
+    m.header['List-Id'] = "<notification errors_mailer:yml_url_not_respond>"
+    m.header['Feedback-ID'] = "#{@shop.customer.email}:yml_url_not_respond:errors_mailer:rees46mailer"
+    m
+  end
+
+  def yml_syntax_error(shop, message)
+    @shop = shop
+    @message = message
+    I18n.locale = @shop.customer.language || 'en'
+    @manager = shop.manager || Customer.default_manager(I18n.locale.to_s)
+
+    m = mail(from: @manager.email, to: @shop.customer.email, bcc: @manager.email, subject: I18n.t('errors_mailer.subject.yml_syntax_error'))
+    m.header['List-Id'] = "<notification errors_mailer:yml_syntax_error>"
+    m.header['Feedback-ID'] = "#{@shop.customer.email}:yml_syntax_error:errors_mailer:rees46mailer"
+    m
+  end
+
+  def yml_off(shop)
+    @shop = shop
+    I18n.locale = @shop.customer.language || 'en'
+    @manager = shop.manager || Customer.default_manager(I18n.locale.to_s)
+
+    m = mail(from: @manager.email, to: @shop.customer.email, bcc: @manager.email, subject: I18n.t('errors_mailer.subject.yml_off'))
+    m.header['List-Id'] = "<notification errors_mailer:yml_off>"
+    m.header['Feedback-ID'] = "#{@shop.customer.email}:yml_off:errors_mailer:rees46mailer"
+    m
+  end
+
+
+  ## Orders Import Errors
+
   def orders_import_error(email, message, params)
     mail(to: email, bcc: [], subject: "Ошибка при импорте заказов") do |format|
       format.text { render text: "#{message}\n\n\n#{params.inspect}" }
       format.html { render text: "#{message}<hr/>#{params.inspect}" }
     end
   end
-
-  def yml_import_error(shop, reason)
-    manager =  shop.manager.present? ? shop.manager : Customer.default_manager
-    mail(from: manager.email, to: shop.customer.email, bcc: manager.email, subject: 'Неправильно указан YML файл') do |format|
-      format.html { "<p>Здравствуйте!</p><p>К сожалению мы не смогли обработать YML-файл вашего интернет-магазина  <a href='#{shop.url}'>#{shop.name}</a>. <br />В процессе обработки возникла следующая ошибка: #{reason}</p><p>URL файла: <a href='#{shop.yml_file_url}' target='_blank'>#{shop.yml_file_url}</a></p><p>Пожалуйста, проверьте корректность YML-файла и при необходимости измените ссылку на YML-файл в настройках магазина в <a href='#{Rees46.site_url}/shops/#{shop.id}/edit'>личном кабинете</a>.</p><p>Если на момент получения данного письма вы уже внесли все необходимые изменения и YML-файл доступен, то просто проигнорируйте это сообщение.</p><p>На все ваши вопросы ответит ваш менеджер #{manager.name}. <br /><a href='http://rees46.com' target='_blank'>rees46.com</a> | <a href='mailto:#{manager.email}' target='_blank'>#{manager.email}</a> | +7 (812) 980-45-33 </p>" }
-    end
-  end
-
-  def yml_url_not_respond(shop)
-    manager =  shop.manager.present? ? shop.manager : Customer.default_manager
-    mail(from: manager.email, to: shop.customer.email, bcc: manager.email, subject: 'Не удалось загрузить YML-файл') do |format|
-      format.html { "<p>Здравствуйте!</p><p>К сожалению мы не смогли загрузить YML-файл вашего интернет-магазина <a href='#{shop.url}'>#{shop.name}</a>. <br />Пожалуйста, проверьте правильность ссылки, указанной в <a href='#{Rees46.site_url}/shops/#{shop.id}/edit'>личном кабинете</a>.</p><p>Если на момент получения данного письма вы уже внесли все необходимые изменения и YML-файл доступен, то просто проигнорируйте это сообщение.</p><p>На все ваши вопросы ответит ваш менеджер #{manager.name}. <br /><a href='http://rees46.com' target='_blank'>rees46.com</a> | <a href='mailto:#{manager.email}' target='_blank'>#{manager.email}</a> | +7 (812) 980-45-33 </p>" }
-    end
-  end
-
-  def yml_syntax_error(shop, message)
-    manager =  shop.manager.present? ? shop.manager : Customer.default_manager
-    mail(from: manager.email, to: shop.customer.email, bcc: manager.email, subject: 'Ошибка синтаксиса YML-файла') do |format|
-      format.html { "<p>Здравствуйте!</p><p>К сожалению мы не смогли обработать YML-файл вашего интернет-магазина <a href='#{shop.url}'>#{shop.name}</a>. <br />Причина: <strong>#{message}</strong></p><p>Пожалуйста, укажите ссылку на корректный YML-файл в <a href='#{Rees46.site_url}/shops/#{shop.id}/edit'>личном кабинете</a>.</p><p>На все ваши вопросы ответит ваш менеджер #{manager.name}. <br /><a href='http://rees46.com' target='_blank'>rees46.com</a> | <a href='mailto:#{manager.email}' target='_blank'>#{manager.email}</a> | +7 (812) 980-45-33 </p>" }
-    end
-  end
-
-  def yml_off(shop)
-        manager =  shop.manager.present? ? shop.manager : Customer.default_manager
-    mail(from: manager.email, to: shop.customer.email, bcc: manager.email, subject: 'Обработка YML-файла отключена') do |format|
-      format.html { "<p>Здравствуйте!</p><p>К сожалению мы не смогли загрузить YML-файл вашего интернет-магазина <a href='#{shop.url}'>#{shop.name}</a> в течение 5-ти дней. </br />Пожалуйста, проверьте правильность ссылки, указанной в <a href='#{Rees46.site_url}/shops/#{shop.id}/edit'>личном кабинете</a>.</p><p>Если на момент получения данного письма вы уже внесли все необходимые изменения и YML-файл доступен, то просто проигнорируйте это сообщение.</p><p>На все ваши вопросы ответит ваш менеджер #{manager.name}. <br /><a href='http://rees46.com' target='_blank'>rees46.com</a> | <a href='mailto:#{manager.email}' target='_blank'>#{manager.email}</a> | +7 (812) 980-45-33 </p>" }
-    end
-  end
-
 
   # Сообщаем о полной или частичной обработке импорта заказов
   def orders_import_processed(shop, errors)
@@ -81,10 +101,6 @@ class ErrorsMailer < ActionMailer::Base
         }
       end
     end
-
-
-
   end
-
 
 end
