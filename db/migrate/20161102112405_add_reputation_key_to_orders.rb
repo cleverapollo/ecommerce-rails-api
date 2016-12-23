bce229cca7b2e23ff3eb01f6920dd7a535b9d491
cleverapollo <cleverapollo@gmail.com>
2016-12-23 18:19:16 +0300

    new class Order in migration

--- db/migrate/20161102112405_add_reputation_key_to_orders.rb
+++ db/migrate/20161102112405_add_reputation_key_to_orders.rb
@@ -1,18 +1,16 @@
 class AddReputationKeyToOrders < ActiveRecord::Migration
-  class Order < ActiveRecord::Base
-  end
 
   def up
     add_column :orders, :reputation_key, :string
 
-    order_all = Order.count
-    order_counter = 0
+    # order_all = Order.count
+    # order_counter = 0
 
-    Order.find_each do |order|
-      order.update(reputation_key: Digest::MD5.hexdigest(order.id.to_s))
-      order_counter += 1
-      p "обработано #{order_counter} из #{order_all}"
-    end
+    # Order.find_each do |order|
+    #   order.update(reputation_key: Digest::MD5.hexdigest(order.id.to_s))
+    #   order_counter += 1
+    #   p "обработано #{order_counter} из #{order_all}"
+    # end
   end
 
   def down
