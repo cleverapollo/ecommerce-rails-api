7847d21be49b5401f5617ffbbdac7307db84efa6
cleverapollo <cleverapollo@gmail.com>
2017-02-08 13:25:09 +0300

    Save carts to RTB jobs

--- app/services/rtb/broker.rb
+++ app/services/rtb/broker.rb
@@ -14,33 +14,60 @@ module Rtb
     def notify(user, items)
       return false unless feature_available?
       return false unless items.respond_to? :each
-      popunder_item = nil
-      popunder_job = nil
-      items.each do |item|
-        # Комиссия с продажи такого товара – 46 рублей
-        if item.price && item.price >= @customer.currency.min_payment && item.widgetable? && item.is_available? && item.name.to_s.length < 255 && item.image_url.to_s.length < 255 && item.url.to_s.length < 255
-          if rtb_item = RtbJob.find_by(shop_id: shop.id, user_id: user.id, item_id: item.id)
-            rtb_item.update counter: 0, date: Date.current, price: item.price, image: item.image_url, name: item.name, currency: shop.currency, url: item.url
-          else
-            begin
-              rtb_item = RtbJob.create! shop_id: shop.id, user_id: user.id, item_id: item.id, date: Date.current, counter: 0, price: item.price, image: item.image_url, name: item.name, currency: shop.currency, url: item.url, logo: shop.fetch_logo_url
-            rescue ActiveRecord::RecordNotUnique
-              # Concurrency
-              rtb_item = RtbJob.find_by(shop_id: shop.id, user_id: user.id, item_id: item.id)
-            end
-          end
-          if popunder_item.nil? || popunder_item.price < item.price
-            popunder_item = item
-            popunder_job = rtb_item
-          end
+
+      # Оставляем товары, с которыми можно работать
+      items = items.select { |item| item.price && item.price >= @customer.currency.min_payment && item.widgetable? && item.is_available? && item.name.to_s.length < 1024 && item.image_url.to_s.length < 1024 && item.url.to_s.length < 1024 }.compact
+
+      # Переводим в массив с нужными данными
+      items = items.map { |item| {id: item.id, uniqid: item.uniqid, price: item.price.round, image: item.image_url, url: item.url, name: item.name, currency: shop.currency } }
+
+      # Ничего не осталось
+      return false unless items.any?
+
+      # Legacy - оставляем один самый дорогой товар для временного сохранения старой функциональности по баннерам
+      item =  if items.count > 1
+        items.sort { |a,b| a[:price] <=> b[:price] }.first
+      else
+        items.first
+      end
+
+      if job = RtbJob.active_for_user(user).where(shop_id: shop.id).first
+        job.update counter: 0, date: Date.current, price: item[:price], image: item[:image], name: item[:name], currency: item[:currency], url: item[:url], products: items
+      else
+        begin
+          job = RtbJob.create! shop_id: shop.id, user_id: user.id, item_id: item[:id], date: Date.current, counter: 0, price: item[:price], image: item[:image], name: item[:name], currency: item[:currency], url: item[:url], logo: shop.fetch_logo_url, products: items
+        rescue  ActiveRecord::RecordNotUnique
+          job = RtbJob.active_for_user(user).where(shop_id: shop.id).first
         end
       end
-      if popunder_item && popunder_enabled?
-        url = "http://zloe.net/?shop_id=#{shop.uniqid}&item_id=#{popunder_item.uniqid}&source_id=#{popunder_job.id}"
-        ["//my.rtmark.net/img.gif?partner=990&ttl=86400&f=nosync&rurl=#{CGI::escape(url)}"]
-      else
-        return nil
-      end
+
+      # items.each do |item|
+      #   # Комиссия с продажи такого товара – 46 рублей
+      #   if item.price && item.price >= @customer.currency.min_payment && item.widgetable? && item.is_available? && item.name.to_s.length < 255 && item.image_url.to_s.length < 255 && item.url.to_s.length < 255
+      #     if rtb_item = RtbJob.find_by(shop_id: shop.id, user_id: user.id, item_id: item.id)
+      #       rtb_item.update counter: 0, date: Date.current, price: item.price, image: item.image_url, name: item.name, currency: shop.currency, url: item.url
+      #     else
+      #       begin
+      #         rtb_item = RtbJob.create! shop_id: shop.id, user_id: user.id, item_id: item.id, date: Date.current, counter: 0, price: item.price, image: item.image_url, name: item.name, currency: shop.currency, url: item.url, logo: shop.fetch_logo_url
+      #       rescue ActiveRecord::RecordNotUnique
+      #         # Concurrency
+      #         rtb_item = RtbJob.find_by(shop_id: shop.id, user_id: user.id, item_id: item.id)
+      #       end
+      #     end
+      #     if popunder_item.nil? || popunder_item.price < item.price
+      #       popunder_item = item
+      #       popunder_job = rtb_item
+      #     end
+      #   end
+      # end
+      #
+      # if popunder_item && popunder_enabled?
+      #   url = "http://zloe.net/?shop_id=#{shop.uniqid}&item_id=#{popunder_item.uniqid}&source_id=#{popunder_job.id}"
+      #   ["//my.rtmark.net/img.gif?partner=990&ttl=86400&f=nosync&rurl=#{CGI::escape(url)}"]
+      # else
+      #   return nil
+      # end
+
     end
 
 
