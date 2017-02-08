70fd1657d4abf1779bab87edccf9a4fe68ad81e7
cleverapollo <cleverapollo@gmail.com>
2017-02-08 09:21:24 +0300

    trigger mailing add theme columns

--- db/schema.rb
+++ db/schema.rb
@@ -623,12 +623,16 @@ ActiveRecord::Schema.define(version: 20170202144806) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "liquid_template"
-    t.integer  "amount_of_recommended_items",             default: 9,     null: false
     t.string   "mailchimp_campaign_id"
     t.datetime "activated_at"
-    t.integer  "images_dimension",                        default: 3
+    t.integer  "amount_of_recommended_items",             default: 9,     null: false
+    t.integer  "images_dimension",                        default: 3,     null: false
+    t.integer  "theme_id",                    limit: 8
+    t.string   "theme_type"
+    t.jsonb    "template_data"
   end
 
+  add_index "trigger_mailings", ["shop_id", "theme_id", "theme_type"], name: "index_trigger_mailings_theme", using: :btree
   add_index "trigger_mailings", ["shop_id", "trigger_type"], name: "index_trigger_mailings_on_shop_id_and_trigger_type", unique: true, using: :btree
 
   create_table "trigger_mails", id: :bigserial, force: :cascade do |t|
