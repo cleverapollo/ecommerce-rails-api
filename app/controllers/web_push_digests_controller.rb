b94aa367bc54deb19714e8aba03df7bcda7c8240
cleverapollo <cleverapollo@gmail.com>
2017-02-09 11:24:40 +0300

    remove preview digest web push

--- app/controllers/web_push_digests_controller.rb
+++ app/controllers/web_push_digests_controller.rb
@@ -4,19 +4,6 @@
 class WebPushDigestsController < ApplicationController
   include ShopAuthenticator
 
-  # Отправляет тестовое сообщение
-  def send_test
-    web_push_digest = WebPushDigest.find(params.fetch('id'))
-
-    session = Session.find_by code: params[:ssid]
-    client = Client.find_by(shop_id: @shop.id, user_id: session.user_id)
-
-    # Отправляем сообщение
-    WebPush::DigestMessage.new(client, web_push_digest, nil, @shop.web_push_subscriptions_settings.safari_config, true).send
-
-    render nothing: true
-  end
-
   # Запустить рассылку.
   def launch
     WebPushDigestLaunchWorker.perform_async(params)
