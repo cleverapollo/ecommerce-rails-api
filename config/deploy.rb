b67fdf3b28a95ac86e5bf4477964496490ac32d2
cleverapollo <cleverapollo@gmail.com>
2017-04-03 09:40:32 +0300

    sidekiq

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,29 +43,25 @@ after 'deploy:publishing', 'deploy:restart'
 namespace :sidekiq do
   task :start do
     on roles(:app), in: :sequence, wait: 5 do
-      # execute "sudo /etc/init.d/sidekiq.api.rees46 start"
       execute 'sudo /bin/systemctl start sidekiq.api.rees46.long.service'
-      execute 'sudo /bin/systemctl start sidekiq.api.rees46.mailing1.service'
-      execute 'sudo /bin/systemctl start sidekiq.api.rees46.mailing2.service'
+      # execute 'sudo /bin/systemctl start sidekiq.api.rees46.mailing1.service'
+      # execute 'sudo /bin/systemctl start sidekiq.api.rees46.mailing2.service'
       execute 'sudo /bin/systemctl start sidekiq.api.rees46.service'
     end
   end
   task :stop do
     on roles(:app), in: :sequence, wait: 5 do
-      # execute "sudo /etc/init.d/sidekiq.api.rees46 stop"
       execute 'sudo /bin/systemctl stop sidekiq.api.rees46.long.service'
-      execute 'sudo /bin/systemctl stop sidekiq.api.rees46.mailing1.service'
-      execute 'sudo /bin/systemctl stop sidekiq.api.rees46.mailing2.service'
+      # execute 'sudo /bin/systemctl stop sidekiq.api.rees46.mailing1.service'
+      # execute 'sudo /bin/systemctl stop sidekiq.api.rees46.mailing2.service'
       execute 'sudo /bin/systemctl stop sidekiq.api.rees46.service'
     end
   end
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # execute "sudo /etc/init.d/sidekiq.api.rees46 stop"
-      # execute "sudo /etc/init.d/sidekiq.api.rees46 start"
       execute 'sudo /bin/systemctl restart sidekiq.api.rees46.long.service'
-      execute 'sudo /bin/systemctl restart sidekiq.api.rees46.mailing1.service'
-      execute 'sudo /bin/systemctl restart sidekiq.api.rees46.mailing2.service'
+      # execute 'sudo /bin/systemctl restart sidekiq.api.rees46.mailing1.service'
+      # execute 'sudo /bin/systemctl restart sidekiq.api.rees46.mailing2.service'
       execute 'sudo /bin/systemctl restart sidekiq.api.rees46.service'
     end
   end
