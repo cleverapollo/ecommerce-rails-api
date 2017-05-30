4dfad708042bd004d7c5b0eb7c9d1b0bb9b83d9d
cleverapollo <cleverapollo@gmail.com>
2017-05-30 13:53:12 +0300

    fix mytoys digest

--- app/services/digest_mailings/mytoys.rb
+++ app/services/digest_mailings/mytoys.rb
@@ -29,29 +29,25 @@ module DigestMailings
 
             DigestMailingRecommendationsCalculator.open(shop, recommendations_count) do |calculator|
 
-              Slavery.on_slave do
-                clients = shop.clients.suitable_for_digest_mailings.includes(:user)
+              clients = shop.clients.suitable_for_digest_mailings.includes(:user)
 
-                # Для юзера без истории и профиля здесь будем хранить дефолтный набор рекомендаций, чтобы каждый раз его не рассчитывать
-                empty_user_recommendations = nil
+              # Для юзера без истории и профиля здесь будем хранить дефолтный набор рекомендаций, чтобы каждый раз его не рассчитывать
+              empty_user_recommendations = nil
 
-                clients.find_each do |client|
-                  Slavery.on_master do
-                    if IncomingDataTranslator.email_valid?(client.email)
+              clients.find_each do |client|
+                if IncomingDataTranslator.email_valid?(client.email)
 
-                      # Для юзера без истории и профиля здесь будем использовать дефолтный набор рекомендаций, чтобы каждый раз его не рассчитывать
-                      if !client.user.orders.where(shop_id: 828).exists? && (client.user.children.nil? || (client.user.children.is_a?(Array) && client.user.children.empty?))
-                        if empty_user_recommendations.nil?
-                          empty_user_recommendations = calculator.recommendations_for(client.user).map { |r| r.uniqid }
-                        end
-                        recommendations = empty_user_recommendations
-                      else
-                        recommendations = calculator.recommendations_for(client.user).map { |r| r.uniqid }
-                      end
-
-                      file_source.puts "#{client.email};#{recommendations.join(',')}"
+                  # Для юзера без истории и профиля здесь будем использовать дефолтный набор рекомендаций, чтобы каждый раз его не рассчитывать
+                  if !client.user.orders.where(shop_id: 828).exists? && (client.user.children.nil? || (client.user.children.is_a?(Array) && client.user.children.empty?))
+                    if empty_user_recommendations.nil?
+                      empty_user_recommendations = calculator.recommendations_for(client.user).map { |r| r.uniqid }
                     end
+                    recommendations = empty_user_recommendations
+                  else
+                    recommendations = calculator.recommendations_for(client.user).map { |r| r.uniqid }
                   end
+
+                  file_source.puts "#{client.email};#{recommendations.join(',')}"
                 end
               end
             end
