8849fd416e1d7d0bc7c1d049d2d65fbfadebb8b5
cleverapollo <cleverapollo@gmail.com>
2017-06-01 13:37:44 +0300

    stupid test

---
+++ spec/models/item_category_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe ItemCategory do
+  let!(:shop) { create(:shop) }
+
+  context 'validates' do
+    it 'external_id' do
+      params = {shop_id: shop.id, name: 'test'}
+      record = ItemCategory.new(params)
+      expect(record.valid?).to be_falsey
+      expect(record.errors.full_messages.join(', ')).to match('Category id can\'t be blank')
+    end
+
+    it 'insert_or_update' do
+      params = {shop_id: shop.id, name: 'test'}
+      expect{ ItemCategory.insert_or_update(params) }.to raise_error("Category id can't be blank, params: #{params.to_json}")
+    end
+  end
+end
