a74fe6ac07846d8fbe17b4e3f59f8cda52bb4ffc
cleverapollo <cleverapollo@gmail.com>
2017-06-12 17:24:42 +0300

    Disable kids industrial for dochki

--- app/services/recommender/impl/also_bought.rb
+++ app/services/recommender/impl/also_bought.rb
@@ -13,8 +13,10 @@ module Recommender
 
         # Только если есть дети с полом и пол только один (чтобы исключить товары противоположного пола)
         # Тест на дочках показывает снижение продаж. Проверка.
-        if params.industrial_kids && shop.has_products_kids? && user.try(:children).present? && user.children.is_a?(Array) && user.children.any? && user.children.map { |kid| kid['gender'] }.compact.uniq.count == 1
-          result = result.where('(is_child IS TRUE AND (child_gender IS NULL OR child_gender = ?)) OR is_child IS NULL', user.children.map { |kid| kid['gender'] }.compact.uniq.first)
+        if shop.id != 725
+          if params.industrial_kids && shop.has_products_kids? && user.try(:children).present? && user.children.is_a?(Array) && user.children.any? && user.children.map { |kid| kid['gender'] }.compact.uniq.count == 1
+            result = result.where('(is_child IS TRUE AND (child_gender IS NULL OR child_gender = ?)) OR is_child IS NULL', user.children.map { |kid| kid['gender'] }.compact.uniq.first)
+          end
         end
 
         # Основная фильтрация
