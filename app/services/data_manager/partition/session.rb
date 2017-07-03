38dfc60bf38b71d7def13dc42e1c642dbee84b96
cleverapollo <cleverapollo@gmail.com>
2017-07-03 13:16:25 +0300

    sessions move to shard

--- app/services/data_manager/partition/session.rb
+++ app/services/data_manager/partition/session.rb
@@ -1,6 +1,6 @@
-# Класс отвечающий за партиции таблиц: users
+# Класс отвечающий за партиции таблиц: sessions
 # @see https://habrahabr.ru/company/oleg-bunin/blog/309330/
-class DataManager::Partition::User
+class DataManager::Partition::Session
 
   # Количество строк в партиции
   # !!! НЕ ИЗМЕНЯТЬ !!! Если на проде уже созданы партиции
@@ -8,7 +8,7 @@ class DataManager::Partition::User
 
   class << self
 
-    # Проверяет партиции таблицы users. При необходимости создает новые партиции.
+    # Проверяет партиции таблицы sessions. При необходимости создает новые партиции.
     def check
       ActiveRecord::Base.logger.level = 0
 
@@ -16,35 +16,35 @@ class DataManager::Partition::User
       partitions_size.times do |i|
         ActiveRecord::Base.connection.execute <<-SQL
           CREATE TABLE IF NOT EXISTS #{table_name(i)} (
-            LIKE users INCLUDING ALL,
+            LIKE sessions INCLUDING ALL,
             CHECK ( id > #{i * PACK} AND id <= #{(i + 1) * PACK} )
-          ) INHERITS (users);
+          ) INHERITS (sessions);
         SQL
       end
 
       # Создаем триггер, который определяет, куда ему вставлять данные
       ActiveRecord::Base.connection.execute <<-SQL
-        CREATE OR REPLACE FUNCTION users_insert_to(new users) RETURNS bigint AS $$
+        CREATE OR REPLACE FUNCTION sessions_insert_to(new sessions) RETURNS bigint AS $$
           DECLARE in_table text;
           DECLARE i int;
           DECLARE result bigint;
           BEGIN
             i := (ceil(NEW.id::float / #{PACK}) - 1) * 100;
-            in_table := format('users_%s_%s', i, i + 100);
+            in_table := format('sessions_%s_%s', i, i + 100);
             EXECUTE 'INSERT INTO ' || in_table || ' VALUES ( ($1).* ) ' USING NEW;
             RETURN NEW.id;
           END;
         $$ LANGUAGE plpgsql;
-        CREATE OR REPLACE RULE users_insert AS ON INSERT TO users DO INSTEAD ( SELECT users_insert_to(NEW) );
+        CREATE OR REPLACE RULE sessions_insert AS ON INSERT TO sessions DO INSTEAD ( SELECT sessions_insert_to(NEW) );
       SQL
     end
 
-    # !!! Удаляет все партиции таблицы users
+    # !!! Удаляет все партиции таблицы sessions
     def drop_partitions
       ActiveRecord::Base.logger.level = 0
       ActiveRecord::Base.connection.execute <<-SQL
-        DROP RULE IF EXISTS users_insert ON users;
-        DROP FUNCTION IF EXISTS users_insert_to();
+        DROP RULE IF EXISTS sessions_insert ON sessions;
+        DROP FUNCTION IF EXISTS sessions_insert_to();
       SQL
       partitions_size.times do |i|
         ActiveRecord::Base.connection.execute "DROP TABLE IF EXISTS #{table_name(i)} CASCADE"
@@ -52,11 +52,11 @@ class DataManager::Partition::User
     end
 
     # Переносит юзеров с мастера на шард
-    def move_users_from_master
-      min = User.connection.select_value('SELECT min(id) FROM users_1').to_i
-      max = User.connection.select_value('SELECT max(id) FROM users_1').to_i
-      (min..max).step(10000) do |n|
-        ActiveRecord::Base.connection.execute "with moved_rows AS ( delete from users_1 where id < #{(n + 10000)} returning * ) insert into users select * from moved_rows;"
+    def move_from_master
+      min = Session.connection.select_value('SELECT min(id) FROM sessions_master').to_i
+      max = Session.connection.select_value('SELECT max(id) FROM sessions_master').to_i
+      (min..max).step(5000) do |n|
+        ActiveRecord::Base.connection.execute "with moved_rows AS ( delete from sessions_master where id < #{(n + 5000)} AND updated_at IS NOT NULL returning * ) insert into sessions select * from moved_rows;"
         STDOUT.write "\r#{n}"
       end
       STDOUT.write "\n"
@@ -66,12 +66,12 @@ class DataManager::Partition::User
 
     # Сколько должно быть партиций
     def partitions_size
-      (User.maximum(:id).to_f / PACK).ceil.to_i + 1
+      (Session.maximum(:id).to_f / PACK).ceil.to_i + 1
     end
 
     # Имя таблицы для партиции
     def table_name(i)
-      "users_#{(i.to_f * PACK / (PACK / 100)).round.to_i}_#{(((i + 1).to_f * PACK) / (PACK / 100)).round.to_i}"
+      "sessions_#{(i.to_f * PACK / (PACK / 100)).round.to_i}_#{(((i + 1).to_f * PACK) / (PACK / 100)).round.to_i}"
     end
 
   end
