83b8ac5717025eb378f59b4b75ee466b9f6fc38f
cleverapollo <cleverapollo@gmail.com>
2017-07-05 10:42:34 +0300

    experiment for krasotka

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -26,104 +26,139 @@ class RecommendationsController < ApplicationController
     # Запускаем процессор с извлеченными данными
     recommendations = Recommendations::Processor.process(extracted_params)
 
-    # Дочки - если запра
-    if @shop.id == 725 && extracted_params.type == 'also_bought'
-      extracted_params.limit = 8
-      if extracted_params.cart_item_ids && extracted_params.item_id && extracted_params.cart_item_ids.include?(extracted_params.item_id)
-        # Показываем also_bought
-        extracted_params.max_price_filter = 1500
-        extracted_params.type = 'also_bought'
-        extracted_params.track_recommender = false
-        recommendations = Recommendations::Processor.process(extracted_params)
-        extracted_params.max_price_filter = nil
-      else
-        # Показываем similar
-        extracted_params.type = 'similar'
-        extracted_params.track_recommender = false
-        recommendations = Recommendations::Processor.process(extracted_params)
-      end
 
-    end
+    # Эксперимент для Красотки - главная страница
+    if @shop.id == 2413 && extracted_params.type == 'popular' && extracted_params.categories.empty?
 
-    # Эксперимент в корзине дочек - A - показываем recently_viewed, B - показываем see_also с ограничением по цене
-    if @shop.id == 725 && extracted_params.type == 'see_also'
-      if extracted_params.segments && extracted_params.segments.first == '1_0'
-        extracted_params.type = 'recently_viewed'
-        _extracted = extracted_params.exclude
-        extracted_params.exclude += Item.where(id: extracted_params.cart_item_ids).pluck(:uniqid) if extracted_params.cart_item_ids.any?
-        extracted_params.track_recommender = false
-        recommendations = Recommendations::Processor.process(extracted_params)
-        extracted_params.exclude = _extracted
+      # Оставляем только 3 популярных товара
+      recommendations = recommendations[0..2]
 
-        if recommendations.count < extracted_params.limit
-          extracted_params.max_price_filter = 1500
-          extracted_params.type = 'see_also'
-          extracted_params.track_recommender = false
-          recommendations += Recommendations::Processor.process(extracted_params)
-          extracted_params.max_price_filter = nil
-        end
-      else
-        extracted_params.max_price_filter = 1500
-        extracted_params.type = 'see_also'
-        extracted_params.track_recommender = false
-        recommendations += Recommendations::Processor.process(extracted_params)
-        extracted_params.max_price_filter = nil
-      end
+      experiment_params = extracted_params
 
-    end
+      # Добавляем 3 популярных лака
+      experiment_params.track_recommender = false
+      experiment_params.limit = 3
+      experiment_params.categories = ['514']
+      experiment_params.exclude += recommendations
+      recommendations += Recommendations::Processor.process(experiment_params)
+      recommendations.uniq!
 
-    if @shop.id == 725
-      extracted_params.track_recommender = false
+      # Добавляем 3 сопутствующих к корзине
+      experiment_params.categories = []
+      experiment_params.limit = 3
+      experiment_params.exclude += recommendations
+      experiment_params.type = 'see_also'
+      recommendations += Recommendations::Processor.process(experiment_params)
+      recommendations.uniq!
 
-      # Если нашли меньше, чем нужно для see_also, also_bought
-      if recommendations.count < extracted_params.limit && %w(see_also also_bought).include?(extracted_params.type)
-        extracted_params.skip_niche_algorithms = true
+      # Добиваем популярными
+      if recommendations.count < extracted_params.limit
+        extracted_params.limit = extracted_params.limit - recommendations.count
         extracted_params.exclude += recommendations
-
-        # Запускаем процессор с извлеченными данными
         recommendations += Recommendations::Processor.process(extracted_params)
       end
 
-      # Если нашли меньше, чем нужно для also_bought
-      if recommendations.count < extracted_params.limit && %w(also_bought).include?(extracted_params.type)
 
-        # достаем товары из корзины
-        extracted_params.exclude += recommendations
-
-        # Создаем рекомендер
-        recommender = Recommender::Impl::AlsoBought.new(extracted_params)
-        recommender.use_cart = true
-
-        # Запускаем процессор с извлеченными данными
-        recommendations += recommender.recommendations
-      end
-
-      # Если нашли меньше, чем нужно для see_also, also_bought
-      if recommendations.count < extracted_params.limit && %w(see_also also_bought).include?(extracted_params.type)
-        extracted_params.skip_niche_algorithms = true
-        extracted_params.exclude += recommendations
-
-        # Создаем рекомендер
-        recommender = Recommender::Impl::AlsoBought.new(extracted_params)
-        recommender.use_cart = true
-
-        # Запускаем процессор с извлеченными данными
-        recommendations += recommender.recommendations
-      end
-
-      # Если нашли меньше, чем нужно для see_also, also_bought добавляем похожие
-      if recommendations.count < extracted_params.limit && %w(see_also also_bought).include?(extracted_params.type) && shop.id != 2413
-        extracted_params.type = 'similar'
-        extracted_params.exclude += recommendations
-        extracted_params.exclude = extracted_params.exclude.uniq
-
-        # Запускаем процессор с извлеченными данными
-        recommendations += Recommendations::Processor.process(extracted_params)
-      end
-
-      # Обрзаем возможные лишние товары
-      recommendations = recommendations.uniq.take(extracted_params.limit)
     end
+
+    # # Дочки - если запра
+    # if @shop.id == 725 && extracted_params.type == 'also_bought'
+    #   extracted_params.limit = 8
+    #   if extracted_params.cart_item_ids && extracted_params.item_id && extracted_params.cart_item_ids.include?(extracted_params.item_id)
+    #     # Показываем also_bought
+    #     extracted_params.max_price_filter = 1500
+    #     extracted_params.type = 'also_bought'
+    #     extracted_params.track_recommender = false
+    #     recommendations = Recommendations::Processor.process(extracted_params)
+    #     extracted_params.max_price_filter = nil
+    #   else
+    #     # Показываем similar
+    #     extracted_params.type = 'similar'
+    #     extracted_params.track_recommender = false
+    #     recommendations = Recommendations::Processor.process(extracted_params)
+    #   end
+    #
+    # end
+    #
+    # # Эксперимент в корзине дочек - A - показываем recently_viewed, B - показываем see_also с ограничением по цене
+    # if @shop.id == 725 && extracted_params.type == 'see_also'
+    #   if extracted_params.segments && extracted_params.segments.first == '1_0'
+    #     extracted_params.type = 'recently_viewed'
+    #     _extracted = extracted_params.exclude
+    #     extracted_params.exclude += Item.where(id: extracted_params.cart_item_ids).pluck(:uniqid) if extracted_params.cart_item_ids.any?
+    #     extracted_params.track_recommender = false
+    #     recommendations = Recommendations::Processor.process(extracted_params)
+    #     extracted_params.exclude = _extracted
+    #
+    #     if recommendations.count < extracted_params.limit
+    #       extracted_params.max_price_filter = 1500
+    #       extracted_params.type = 'see_also'
+    #       extracted_params.track_recommender = false
+    #       recommendations += Recommendations::Processor.process(extracted_params)
+    #       extracted_params.max_price_filter = nil
+    #     end
+    #   else
+    #     extracted_params.max_price_filter = 1500
+    #     extracted_params.type = 'see_also'
+    #     extracted_params.track_recommender = false
+    #     recommendations += Recommendations::Processor.process(extracted_params)
+    #     extracted_params.max_price_filter = nil
+    #   end
+    #
+    # end
+    #
+    # if @shop.id == 725
+    #   extracted_params.track_recommender = false
+    #
+    #   # Если нашли меньше, чем нужно для see_also, also_bought
+    #   if recommendations.count < extracted_params.limit && %w(see_also also_bought).include?(extracted_params.type)
+    #     extracted_params.skip_niche_algorithms = true
+    #     extracted_params.exclude += recommendations
+    #
+    #     # Запускаем процессор с извлеченными данными
+    #     recommendations += Recommendations::Processor.process(extracted_params)
+    #   end
+    #
+    #   # Если нашли меньше, чем нужно для also_bought
+    #   if recommendations.count < extracted_params.limit && %w(also_bought).include?(extracted_params.type)
+    #
+    #     # достаем товары из корзины
+    #     extracted_params.exclude += recommendations
+    #
+    #     # Создаем рекомендер
+    #     recommender = Recommender::Impl::AlsoBought.new(extracted_params)
+    #     recommender.use_cart = true
+    #
+    #     # Запускаем процессор с извлеченными данными
+    #     recommendations += recommender.recommendations
+    #   end
+    #
+    #   # Если нашли меньше, чем нужно для see_also, also_bought
+    #   if recommendations.count < extracted_params.limit && %w(see_also also_bought).include?(extracted_params.type)
+    #     extracted_params.skip_niche_algorithms = true
+    #     extracted_params.exclude += recommendations
+    #
+    #     # Создаем рекомендер
+    #     recommender = Recommender::Impl::AlsoBought.new(extracted_params)
+    #     recommender.use_cart = true
+    #
+    #     # Запускаем процессор с извлеченными данными
+    #     recommendations += recommender.recommendations
+    #   end
+    #
+    #   # Если нашли меньше, чем нужно для see_also, also_bought добавляем похожие
+    #   if recommendations.count < extracted_params.limit && %w(see_also also_bought).include?(extracted_params.type) && shop.id != 2413
+    #     extracted_params.type = 'similar'
+    #     extracted_params.exclude += recommendations
+    #     extracted_params.exclude = extracted_params.exclude.uniq
+    #
+    #     # Запускаем процессор с извлеченными данными
+    #     recommendations += Recommendations::Processor.process(extracted_params)
+    #   end
+    #
+    #   # Обрзаем возможные лишние товары
+    #   recommendations = recommendations.uniq.take(extracted_params.limit)
+    # end
 
     # Для триггера "Брошенная категория" отмечаем подписку на категории
     # Если категории есть, конечно.
