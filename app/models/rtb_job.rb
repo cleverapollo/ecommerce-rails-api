c5db3fd64c293799d2bdd9f4f082f4ee12450676
cleverapollo <cleverapollo@gmail.com>
2017-07-24 22:57:08 +0300

    enlarge time frame

--- app/models/rtb_job.rb
+++ app/models/rtb_job.rb
@@ -5,7 +5,7 @@ class RtbJob < ActiveRecord::Base
   has_many :rtb_impressions, foreign_key: :ad_id
   scope :active, -> { where('active IS TRUE') }
 
-  scope :active_for_user, -> (user) { where(source_user_id: user.id).where('active IS TRUE').where('counter < 10').where('date >= ?', 2.days.ago.beginning_of_day).order(id: :desc) }
+  scope :active_for_user, -> (user) { where(source_user_id: user.id).where('active IS TRUE').where('counter < 50').where('date >= ?', 5.days.ago.beginning_of_day).order(id: :desc) }
 
 
 end
