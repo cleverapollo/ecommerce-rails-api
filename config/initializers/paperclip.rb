37ddac6676ee57fd6fce6897c515936a5ab70f19
cleverapollo <cleverapollo@gmail.com>
2017-09-20 23:55:43 +0300

    paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,3 +3,12 @@ Paperclip::Attachment.default_options.merge!(
   path: ":rails_root/public/uploads/:class/:attachment/:id/:style/:basename.:extension",
   default_url: 'missing.jpg'
 )
+
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
