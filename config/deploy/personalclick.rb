b3cb51cb0182654bf0f4530bf833539f1d1c7b7a
cleverapollo <cleverapollo@gmail.com>
2017-10-09 17:44:39 +0300

    deploy personalclick

---
+++ config/deploy/personalclick.rb
@@ -0,0 +1,18 @@
+role :app, %w{94.130.90.232}
+role :web, %w{94.130.90.232}
+role :db,  %w{94.130.90.232}
+
+set :application, 'api.personalclick.com'
+set :stage, :personalclick
+
+set :log_level,   :info
+
+set :ssh_options, {
+    user: 'rails',
+    forward_agent: true,
+    port: 21212
+}
+
+set :deploy_to, "/home/rails/#{fetch(:application)}"
+set :branch, 'master'
+set :rails_env, 'production'
