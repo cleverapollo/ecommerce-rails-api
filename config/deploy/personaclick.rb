960ce04c956b4bde97f7c73dabf0ea6ef6a95d27
cleverapollo <cleverapollo@gmail.com>
2017-10-16 14:40:32 +0300

    ci

--- config/deploy/personaclick.rb
+++ config/deploy/personaclick.rb
@@ -20,6 +20,38 @@ set :rails_env, 'production'
 Rake::Task['deploy:start'].clear_actions
 Rake::Task['deploy:stop'].clear_actions
 Rake::Task['deploy:restart'].clear_actions
+Rake::Task['sidekiq:start'].clear_actions
+Rake::Task['sidekiq:stop'].clear_actions
+Rake::Task['sidekiq:restart'].clear_actions
+
+
+namespace :sidekiq do
+  task :start do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute 'sudo /bin/systemctl start sidekiq.api.rees46.yml.service'
+      # execute 'sudo /bin/systemctl start sidekiq.api.rees46.mailing1.service'
+      # execute 'sudo /bin/systemctl start sidekiq.api.rees46.mailing2.service'
+      execute 'sudo /bin/systemctl start sidekiq.api.rees46.service'
+    end
+  end
+  task :stop do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute 'sudo /bin/systemctl stop sidekiq.api.rees46.yml.service'
+      # execute 'sudo /bin/systemctl stop sidekiq.api.rees46.mailing1.service'
+      # execute 'sudo /bin/systemctl stop sidekiq.api.rees46.mailing2.service'
+      execute 'sudo /bin/systemctl stop sidekiq.api.rees46.service'
+    end
+  end
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute 'sudo /bin/systemctl restart sidekiq.api.rees46.yml.service'
+      # execute 'sudo /bin/systemctl restart sidekiq.api.rees46.mailing1.service'
+      # execute 'sudo /bin/systemctl restart sidekiq.api.rees46.mailing2.service'
+      execute 'sudo /bin/systemctl restart sidekiq.api.rees46.service'
+    end
+  end
+end
+
 namespace :deploy do
 
   desc 'Start unicorn'
