e949e950d79d49a11e2176915d90e7bc73eef47b
cleverapollo <cleverapollo@gmail.com>
2017-11-24 12:30:23 +0300

    move method fix

--- app/services/data_manager/move_data.rb
+++ app/services/data_manager/move_data.rb
@@ -3,10 +3,13 @@ class DataManager::MoveData
   def self.move(table)
     batch = 1000
     n = 0
-    table.classify.constantize.from("#{table}_master as #{table}").select("#{table}.id").find_in_batches(batch_size: batch) do |group|
+    cl = table.classify.constantize
+    cl.from("#{table}_master as #{table}").select("#{table}.id").find_in_batches(batch_size: batch) do |group|
       ids = group.map(&:id)
       n += ids.size
-      ActiveRecord::Base.connection.execute "with moved_rows AS ( delete from #{table}_master where id IN (#{ids.join(',')}) returning * ) insert into #{table} select * from moved_rows;"
+      cl.transaction do
+        ActiveRecord::Base.connection.execute "with moved_rows AS ( delete from #{table}_master where id IN (#{ids.join(',')}) returning * ) insert into #{table} select * from moved_rows;"
+      end
       STDOUT.write "\r#{n}"
       sleep(1) if n % 50000 == 0
     end
