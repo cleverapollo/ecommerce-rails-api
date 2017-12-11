1fd57e9ed1d5dc78c8aa704130a2f545305ce740
cleverapollo <cleverapollo@gmail.com>
2017-12-11 14:17:54 +0530

    [Fix] full search fuzziness

--- app/services/search_engine/full_search.rb
+++ app/services/search_engine/full_search.rb
@@ -38,7 +38,7 @@ class SearchEngine::FullSearch < SearchEngine::Base
               json.set! x[0] do
                 json.set! x[1], x[2]
                 json.fields ['name', 'brand', 'category_names']
-                json.fuzziness 2
+                json.fuzziness 1
               end
             end
           end
