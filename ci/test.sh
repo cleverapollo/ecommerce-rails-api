b76fc27211e8043ac0a9f9c2a194d64c8e248949
cleverapollo <cleverapollo@gmail.com>
2017-12-22 17:14:51 +0300

    db added

--- ci/test.sh
+++ ci/test.sh
@@ -7,6 +7,7 @@ sed -i "s|localhost|postgres|" "config/database.yml"
 #Wait for pg to come up
 sleep 10
 echo postgres:5432:rees46_test:rails:rails  > ~/.pgpass
+chmod 0600 ~/.pgpass
 
 cp config/secrets.yml.example config/secrets.yml
 bin/testing
