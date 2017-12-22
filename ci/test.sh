00c3ef4cad3ac059e40ac8bb899946e32bc705aa
cleverapollo <cleverapollo@gmail.com>
2017-12-22 17:14:17 +0300

    db added

--- ci/test.sh
+++ ci/test.sh
@@ -6,6 +6,7 @@ sed -i "s|localhost|postgres|" "config/database.yml"
 # psql -c 'create database master_database;'
 #Wait for pg to come up
 sleep 10
+echo postgres:5432:rees46_test:rails:rails  > ~/.pgpass
 
 cp config/secrets.yml.example config/secrets.yml
 bin/testing
