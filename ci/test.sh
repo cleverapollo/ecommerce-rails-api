75c0cc09f0a5a98616a934d7c3187a2219543221
cleverapollo <cleverapollo@gmail.com>
2017-12-22 16:08:18 +0300

    db added

--- ci/test.sh
+++ ci/test.sh
@@ -1,7 +1,7 @@
 bundle install
 export RAILS_ENV=test
 cp config/database.yml.example config/database.yml
-sed -i "s|5432|5435|" "config/database.yml"
+#sed -i "s|localhost|database|" "config/database.yml"
 # psql -c 'create database master_database;'
-psql -p 5435 -c 'create database rees46_clickhouse_test;'
+psql -p 5432 -h database -c 'create database rees46_clickhouse_test;'
 cp config/secrets.yml.example config/secrets.yml
