87d0ea02a8df26e2a5cef1a10b72b771b45bc3bb
cleverapollo <cleverapollo@gmail.com>
2017-12-22 17:30:01 +0300

    db added

--- ci/test.sh
+++ ci/test.sh
@@ -6,7 +6,7 @@ sed -i "s|localhost|postgres|" "config/database.yml"
 # psql -c 'create database master_database;'
 #Wait for pg to come up
 sleep 10
-echo *:5432:*:rails:rails  > ~/.pgpass
+echo "*:5432:*:rails:rails"  > ~/.pgpass
 ls -la ~/.pgpass
 echo ~/.pgpass
 chmod 0600 ~/.pgpass
