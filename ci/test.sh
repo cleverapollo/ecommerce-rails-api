b9f9c7b203ec476a06c44cb6724865eca8d685d1
cleverapollo <cleverapollo@gmail.com>
2017-12-22 16:31:02 +0300

    db added

--- ci/test.sh
+++ ci/test.sh
@@ -3,5 +3,23 @@ export RAILS_ENV=test
 cp config/database.yml.example config/database.yml
 #sed -i "s|localhost|database|" "config/database.yml"
 # psql -c 'create database master_database;'
+#Wait for pg to come up
+function test_postgresql {
+  pg_isready -h postgres
+}
+
+count=0
+# Chain tests together by using &&
+until ( test_postgresql)
+do
+  ((count++))
+  if [ ${count} -gt 50 ]
+  then
+    echo "Services didn't become ready in time"
+    exit 1
+  fi
+  sleep 0.1
+done
 psql -p 5432 -h postgres -c 'create database rees46_clickhouse_test;'
+
 cp config/secrets.yml.example config/secrets.yml
