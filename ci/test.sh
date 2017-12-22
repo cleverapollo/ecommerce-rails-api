aa992f104ffb316fac700b2589bc3f7ed86cc3e2
cleverapollo <cleverapollo@gmail.com>
2017-12-22 17:08:58 +0300

    db added

--- ci/test.sh
+++ ci/test.sh
@@ -2,7 +2,7 @@ bundle install
 export RAILS_ENV=test
 export REES46_SHARD=01
 cp config/database.yml.example config/database.yml
-#sed -i "s|localhost|database|" "config/database.yml"
+sed -i "s|localhost|postgres|" "config/database.yml"
 # psql -c 'create database master_database;'
 #Wait for pg to come up
 sleep 10
