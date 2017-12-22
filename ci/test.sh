b80cad99886c4263f9e2ea3a7146d14ddb4fbbf7
cleverapollo <cleverapollo@gmail.com>
2017-12-22 16:53:25 +0300

    db added

--- ci/test.sh
+++ ci/test.sh
@@ -4,22 +4,7 @@ cp config/database.yml.example config/database.yml
 #sed -i "s|localhost|database|" "config/database.yml"
 # psql -c 'create database master_database;'
 #Wait for pg to come up
-function test_postgresql {
-  pg_isready -h postgres
-}
-
-count=0
-# Chain tests together by using &&
-until ( test_postgresql)
-do
-  ((count++))
-  if [ ${count} -gt 50 ]
-  then
-    echo "Services didn't become ready in time"
-    exit 1
-  fi
-  sleep 1
-done
+sleep 10
 
 cp config/secrets.yml.example config/secrets.yml
 bin/testing
