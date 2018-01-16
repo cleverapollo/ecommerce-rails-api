13158156255acc886c1a1147ae20a72364cb036f
cleverapollo <cleverapollo@gmail.com>
2018-01-16 13:14:25 +0300

    fix user fetcher

--- app/services/user_fetcher.rb
+++ app/services/user_fetcher.rb
@@ -23,35 +23,49 @@ class UserFetcher
   def fetch
     # Сессия должна существовать
     self.session = Session.find_by_code(session_code)
-    raise SessionNotFoundError if self.session.blank?
+    raise SessionNotFoundError if self.session.blank? && email.blank?
 
-    # Находим или создаем связку пользователя с магазином
-    begin
-      # Новая версия, ищем клиента по сессии
-      self.client = Client.find_by(session: session, shop: shop)
-
-      # Поддержка старого метода, пробуем найти юзера, если по сессии не нашли
+    # Если не нашли сессию и есть email ищем клиента (или создаем нового)
+    if session.nil? && email.present?
+      self.client = Client.find_by(shop: shop, email: email)
       if client.nil?
-        self.client = Client.find_by(user: session.user, shop: shop)
+        begin
+          self.client = Client.create!(shop: shop, user: User.create)
+        rescue # Concurrency?
+          self.client =  Client.find_by(shop: shop, email: email)
+        end
+        self.client.update(user: User.create) if client.user.nil?
+        self.session = Session.find_or_create_by!(user_id: client.user_id)
+      end
+    else
+
+      # Находим или создаем связку пользователя с магазином
+      begin
+        # Новая версия, ищем клиента по сессии
+        self.client = Client.find_by(session: session, shop: shop)
+
+        # Поддержка старого метода, пробуем найти юзера, если по сессии не нашли
+        if client.nil?
+          self.client = Client.find_by(user: session.user, shop: shop)
+        end
+
+        # Создаем, если не найден
+        if client.nil?
+          self.client = Client.create!(session: session, shop: shop, user: session.user)
+        elsif client.session_id.blank?
+          client.session_id = session.id
+          client.atomic_save!
+        end
+      rescue ActiveRecord::RecordNotUnique
+        self.client = shop.clients.find_by!(session: session)
       end
 
-      # Создаем, если не найден
-      if client.nil?
-        self.client = Client.create!(session: session, shop: shop, user: session.user)
-      elsif client.session_id.blank?
-        client.session_id = session.id
-        client.atomic_save!
+      if location.present? && (client.location.nil? || client.location != location.to_s)
+        client.location = location
+        client.atomic_save if client.changed?
       end
-    rescue ActiveRecord::RecordNotUnique
-      self.client = shop.clients.find_by!(session: session)
-    end
 
-    if location.present? && (client.location.nil? || client.location != location.to_s)
-      client.location = location
-      client.atomic_save if client.changed?
     end
-
-    user = client.user
 
     # Если указан email, сохраняем его клиенту
     if email.present?
@@ -64,6 +78,6 @@ class UserFetcher
     end
 
 
-    user
+    client.user
   end
 end
